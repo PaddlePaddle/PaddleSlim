20d4282eef2f2c59b029c1e7b611981086a62134
wanghaoshuang <wanghaoshuang@baidu.com>
2020-01-03 14:20:29 +0800

    Remove fast_scnn demo.

--- demo/models/__init__.py
+++ demo/models/__init__.py
@@ -2,6 +2,5 @@ from .mobilenet import MobileNet
 from .resnet import ResNet34, ResNet50
 from .mobilenet_v2 import MobileNetV2
 from .pvanet import PVANet
-from .fast_scnn import FastSCNN
 
-__all__ = ['MobileNet', 'ResNet34', 'ResNet50', 'MobileNetV2', 'PVANet', 'FastSCNN']
+__all__ = ['MobileNet', 'ResNet34', 'ResNet50', 'MobileNetV2', 'PVANet']
