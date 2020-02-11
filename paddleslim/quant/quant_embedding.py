fba73c931f64300a1c9a27482d2c8a9d464f7311
slf12 <slf12thuss@163.com>
2020-02-11 08:46:04 +0000

    add log embedding quantization

--- paddleslim/quant/quant_embedding.py
+++ paddleslim/quant/quant_embedding.py
@@ -18,24 +18,30 @@ from __future__ import print_function
 import logging
 import copy
 import numpy as np
+from multiprocessing.dummy import Pool as ThreadPool
 
 import paddle.fluid as fluid
 from paddle.fluid.framework import IrGraph
 from paddle.fluid import core
+from paddle.fluid.layer_helper import LayerHelper
 
-#_logger = logging.basicConfig(level=logging.DEBUG)
+from ..common import get_logger
+_logger = get_logger(__name__, level=logging.INFO)
 
 __all__ = ['quant_embedding']
 
-default_config = {
-    "quantize_type": "abs_max",
+SUPPORT_QUANTIZE_TYPES = ['abs_max', 'log']
+SUPPORT_OP_TYPES = ['lookup_table', 'fused_embedding_seq_pool']
+SUPPORT_QUANTIZE_BITS = [8]
+SUPPORT_DTYPE = ['int8']
+
+_default_single_config = {
+    "quantize_type": "log",
     "quantize_bits": 8,
     "dtype": "int8"
 }
 
-support_quantize_types = ['abs_max']
-support_quantize_bits = [8]
-support_dtype = ['int8']
+_default_config = {"quantize_op_types": SUPPORT_OP_TYPES, }
 
 
 def _merge_config(old_config, new_config):
@@ -49,32 +55,47 @@ def _merge_config(old_config, new_config):
     """
     old_config.update(new_config)
     keys = old_config.keys()
-    assert 'params_name' in keys, "params_name must be set"
+    assert isinstance(old_config['quantize_op_types'], (str, list)), \
+            'quantize_op_types can only be str or list[str]'
+    if isinstance(old_config['quantize_op_types'], str):
+        old_config['quantize_op_types'] = [old_config['quantize_op_types']]
+    for op_type in old_config['quantize_op_types']:
+        assert op_type in SUPPORT_OP_TYPES, \
+                '{} is not supported, supported op types are {}'.format(
+                        op_type, SUPPORT_OP_TYPES)
+        if op_type not in keys:
+            old_config[op_type] = _default_single_config
+            continue
+        else:
+            assert isinstance(old_config[op_type], dict), \
+                    "op type {}'s config must be dict"
+            config_tmp = copy.deepcopy(_default_single_config)
+            config_tmp.update(old_config[op_type])
+            old_config[op_type] = config_tmp
 
-    quantize_type = old_config['quantize_type']
-    assert isinstance(quantize_type, str), "quantize_type must be \
+        quantize_type = old_config[op_type]['quantize_type']
+        assert isinstance(quantize_type, str), "quantize_type must be \
             str"
 
-    assert quantize_type in support_quantize_types, " \
-            quantize_type {} is not supported, now supported quantize type \
-            are {}.".format(quantize_type, support_quantize_types)
+        assert quantize_type in SUPPORT_QUANTIZE_TYPES , "" \
+            "quantize_type {} is not supported, now supported quantize type" \
+            " are {}.".format(quantize_type, SUPPORT_QUANTIZE_TYPES)
 
-    quantize_bits = old_config['quantize_bits']
-    assert isinstance(quantize_bits, int), "quantize_bits must be int"
-    assert quantize_bits in support_quantize_bits, " quantize_bits {} \
-                is not supported, now supported quantize bits are \
-                {}. ".format(quantize_bits, support_quantize_bits)
+        quantize_bits = old_config[op_type]['quantize_bits']
+        assert isinstance(quantize_bits, int), "quantize_bits must be int"
+        assert quantize_bits in SUPPORT_QUANTIZE_BITS , " quantize_bits {}" \
+                " is not supported, now supported quantize bits are" \
+                " {}. ".format(quantize_bits, SUPPORT_QUANTIZE_BITS)
 
-    dtype = old_config['dtype']
-    assert isinstance(dtype, str), "dtype must be str"
-    assert dtype in support_dtype, " dtype {} is not \
-            supported, now supported dtypes are {} \
-                ".format(dtype, support_dtype)
-    if 'threshold' in keys:
-        assert isinstance(old_config['threshold'], (float, int)), "threshold \
-                must be number."
+        dtype = old_config[op_type]['dtype']
+        assert isinstance(dtype, str), "dtype must be str"
+        assert dtype in SUPPORT_DTYPE , " dtype {} is not "\
+            "supported, now supported dtypes are {} ".format(dtype, SUPPORT_DTYPE)
+        if 'threshold' in old_config[op_type].keys():
+            assert isinstance(old_config[op_type]['threshold'], (float, int)), \
+                    "threshold must be number."
 
-    print("quant_embedding config {}".format(old_config))
+    _logger.info("quant_embedding config {}".format(old_config))
     return old_config
 
 
@@ -109,6 +130,10 @@ def _get_scale_var_name(var_name):
     return var_name + '.scale'
 
 
+def _get_dict_var_name(var_name):
+    return var_name + '.dict'
+
+
 def _get_quant_var_name(var_name):
     """
     get quantized var name
@@ -139,7 +164,8 @@ def _clear_var(var_name, scope):
     tensor._clear()
 
 
-def _quant_embedding_abs_max(graph, scope, place, config):
+def _quant_embedding_abs_max(graph, scope, place, config, var_name,
+                             embedding_node):
     """
     quantize embedding using abs_max
 
@@ -190,10 +216,7 @@ def _quant_embedding_abs_max(graph, scope, place, config):
         for node in output_ops:
             graph.update_input_link(var_node, dequant_var_node, node)
 
-    all_var_nodes = graph.all_var_nodes()
-    var_name = config['params_name']
-    # find embedding var node by 'params_name'
-    embedding_node = graph._find_node_by_name(all_var_nodes, var_name)
+    # find embedding var node by 'var_name'
     embedding_tensor = _get_var_tensor(scope, var_name)
     if 'threshold' in config.keys():
         embedding_tensor = _clip_tensor(embedding_tensor, config['threshold'])
@@ -221,18 +244,186 @@ def _quant_embedding_abs_max(graph, scope, place, config):
 
     # insert dequantize_abs_max op
     for op_node in embedding_node.outputs:
-        if op_node.name() == 'lookup_table':
-            graph.update_input_link(embedding_node, quant_tensor_var, op_node)
-            var_node = op_node.outputs[0]
-            _insert_dequant_abs_max_op(graph, scope, var_node, scale_var,
-                                       config)
+        graph.update_input_link(embedding_node, quant_tensor_var, op_node)
+        var_node = op_node.outputs[0]
+        _insert_dequant_abs_max_op(graph, scope, var_node, scale_var, config)
 
     # free float embedding params memory
     _clear_var(embedding_node.name(), scope)
     graph.safe_remove_nodes(embedding_node)
 
 
-def quant_embedding(program, place, config, scope=None):
+def _quant_embedding_log(graph, scope, place, config, var_name,
+                         embedding_node):
+    """
+    quantize embedding using log
+
+    Args:
+        graph(IrGraph): graph that includes lookup_table op
+        scope(fluid.Scope): scope 
+        place(fluid.CPUPlace or flud.CUDAPlace): place to run program
+        config(dict): config to quant
+    """
+
+    _inverval = 0.125
+    _dict_len = 256
+    _dict = np.zeros(_dict_len)
+
+    def _search(array, num_array):
+        length = len(array)
+        res = np.searchsorted(array, num_array)
+        res_refine = []
+        for i in range(len(num_array)):
+            value = num_array[i]
+            idx = res[i]
+            if idx > 0 and (idx == length) or (
+                    abs(array[idx - 1] - value) < abs(array[idx] - value)):
+                res_refine.append(idx - 1)
+            else:
+                res_refine.append(idx)
+        return np.array(res_refine)
+
+    def _quant_log(tensor_array, config):
+        """
+        quant array using log op
+        """
+        bit_length = config['quantize_bits']
+        assert bit_length == 8, 'log quantization only supports 8 bits'
+        log_and_quant = np.round(np.log2(np.abs(tensor_array)) /
+                                 _inverval) * _inverval
+        unique, counts = np.unique(log_and_quant, return_counts=True)
+        topk_index = counts.argsort()[::-1][0:int(_dict_len / 2)]
+        topk_num = unique[topk_index]
+        topk_num = np.sort(topk_num)
+        log_num = np.log2(np.abs(tensor_array))
+        pool = ThreadPool(8)
+        quanted_array = pool.map(lambda x: np.searchsorted(topk_num, x),
+                                 log_num)
+        quanted_array = np.array(quanted_array)
+        pool.close()
+        pool.join()
+        index_tmp = tensor_array < 0
+        quanted_array_tmp = quanted_array[index_tmp]
+        quanted_array_tmp = quanted_array_tmp - 128
+        quanted_array[index_tmp] = quanted_array_tmp
+        quanted_array = quanted_array.astype(config['dtype'])
+        return topk_num, quanted_array
+
+    def _insert_dequant_log_op(graph, scope, var_node, topk_num_node, config):
+        """
+        Insert dequantize_log op in graph
+        """
+        assert var_node.is_var(), "{} is not a var".format(var_node.name())
+
+        dequant_var_node = graph.create_var_node(
+            name=_get_dequant_var_name(var_node.name()),
+            var_type=var_node.type(),
+            shape=var_node.shape(),
+            var_dtype=core.VarDesc.VarType.FP32)
+        scope.var(dequant_var_node.name())
+
+        output_ops = var_node.outputs
+        dequant_op = graph.create_op_node(
+            op_type='dequantize_log',
+            attrs={'op_role': core.op_proto_and_checker_maker.OpRole.Forward},
+            inputs={'X': var_node,
+                    'Dict': topk_num_node},
+            outputs={'Out': dequant_var_node})
+        graph.link_to(var_node, dequant_op)
+        graph.link_to(topk_num_node, dequant_op)
+        graph.link_to(dequant_op, dequant_var_node)
+        for node in output_ops:
+            graph.update_input_link(var_node, dequant_var_node, node)
+
+    # find embedding var node by 'var_name'
+    embedding_tensor = _get_var_tensor(scope, var_name)
+
+    # get quantize dict and quanted tensor
+    topk_num, quanted_tensor = _quant_log(embedding_tensor, config)
+
+    #create params must use create_persistable_node
+    topk_num_var = graph.create_persistable_node(
+        _get_dict_var_name(var_name),
+        var_type=embedding_node.type(),
+        shape=topk_num.shape,
+        var_dtype=core.VarDesc.VarType.FP32)
+    quant_tensor_var = graph.create_persistable_node(
+        _get_quant_var_name(var_name),
+        var_type=embedding_node.type(),
+        shape=embedding_node.shape(),
+        var_dtype=core.VarDesc.VarType.INT8)
+    # create var in scope
+    scope.var(_get_quant_var_name(var_name))
+    scope.var(_get_dict_var_name(var_name))
+    #set var by tensor array or dict
+    _restore_var(_get_quant_var_name(var_name), quanted_tensor, scope, place)
+    _restore_var(_get_dict_var_name(var_name), topk_num, scope, place)
+
+    # insert dequantize_log op
+    for op_node in embedding_node.outputs:
+        graph.update_input_link(embedding_node, quant_tensor_var, op_node)
+        var_node = op_node.outputs[0]
+        _insert_dequant_log_op(graph, scope, var_node, topk_num_var, config)
+
+    # free float embedding params memory
+    _clear_var(embedding_node.name(), scope)
+    graph.safe_remove_nodes(embedding_node)
+
+
+def _remove_link(in_node, out_node):
+    in_node.remove_output(out_node)
+    out_node.remove_input(in_node)
+
+
+def _split_embedding_seq_pool(graph, op):
+    inputs = op.inputs
+    outputs = op.outputs
+    op_desc = op.node.op()
+    combiner = op_desc.attr("combiner")
+    padding_idx = op_desc.attr("padding_idx")
+    is_sparse = op_desc.attr("is_sparse")
+    ids = graph._find_node_by_name(inputs, op.input('Ids')[0])
+    weight = graph._find_node_by_name(inputs, op.input('W')[0])
+    out = outputs[0]
+    lookup_out = graph.create_var_node(
+        name=ids.name() + '.look_up_table.out',
+        var_type=core.VarDesc.VarType.LOD_TENSOR,
+        shape=[1],
+        var_dtype=weight.dtype())
+    lookup_table_op = graph.create_op_node(
+        op_type='lookup_table',
+        attrs={'is_sparse': is_sparse,
+               'padding_idx': padding_idx},
+        inputs={'W': weight,
+                'Ids': ids},
+        outputs={'Out': lookup_out})
+    _remove_link(ids, op)
+    _remove_link(weight, op)
+    _remove_link(op, out)
+    graph.link_to(ids, lookup_table_op)
+    graph.link_to(weight, lookup_table_op)
+    graph.link_to(lookup_table_op, lookup_out)
+    max_index = graph.create_var_node(
+        name=ids.name() + '.seq_pool_op.max_index',
+        var_type=core.VarDesc.VarType.LOD_TENSOR,
+        shape=[1],
+        var_dtype=weight.dtype())
+
+    seq_pool_op = graph.create_op_node(
+        op_type='sequence_pool',
+        inputs={'X': lookup_out},
+        outputs={'Out': out,
+                 'MaxIndex': max_index},
+        attrs={'pooltype': combiner.upper(),
+               'is_test': True})
+    if combiner == 'max':
+        max_index.stop_gradient = True
+    graph.link_to(lookup_out, seq_pool_op)
+    graph.link_to(seq_pool_op, out)
+    graph.link_to(seq_pool_op, max_index)
+
+
+def quant_embedding(program, place, config=None, scope=None):
     """quantize lookup_table op parameters
 
     Args:
@@ -251,12 +442,38 @@ def quant_embedding(program, place, config, scope=None):
     Returns:
         None
     """
-    assert isinstance(config, dict), "config must be dict"
-    config = _merge_config(copy.deepcopy(default_config), config)
+
+    config = {} if config is None else config
+    config = _merge_config(copy.deepcopy(_default_config), config)
     scope = fluid.global_scope() if scope is None else scope
 
     graph = IrGraph(core.Graph(program.desc), for_test=True)
-    if config['quantize_type'] == 'abs_max':
-        _quant_embedding_abs_max(graph, scope, place, config)
+    quantize_params_map = {}
+    all_op = graph.all_op_nodes()
+    for op in all_op:
+        if op.inputs == [] and op.outputs == []:
+            continue
+        op_type = op.name()
+        if op_type in config['quantize_op_types']:
+            weight_name = op.input('W')[0]
+            if weight_name in quantize_params_map.values():
+                continue
+            embedding_node = graph._find_node_by_name(op.inputs,
+                                                      op.input('W')[0])
+            for op_node in embedding_node.outputs:
+                if op_node.name() == 'fused_embedding_seq_pool':
+                    _split_embedding_seq_pool(graph, op_node)
+            if config[op_type]['quantize_type'] == 'abs_max':
+                _quant_embedding_abs_max(graph, scope, place, config[op_type],
+                                         weight_name, embedding_node)
+                pass
+            elif config[op_type]['quantize_type'] == 'log':
+                _quant_embedding_log(graph, scope, place, config[op_type],
+                                     weight_name, embedding_node)
+                pass
+            quantize_params_map[weight_name] = _get_quant_var_name(weight_name)
+    for op in all_op:
+        if op.name() == 'fused_embedding_seq_pool':
+            graph.safe_remove_nodes(op)
 
     return graph.to_program()
