1b3b314d0f4a6e8dbe50cbb982acbfae634821b1
slf12 <slf12thuss@163.com>
2020-04-07 09:40:51 +0000

    remove debug code

--- demo/quant/quant_aware/dsq.py
+++ demo/quant/quant_aware/dsq.py
@@ -4,10 +4,9 @@ from paddle.fluid.layer_helper import LayerHelper
 
 
 def dsq_round(x):
-    #delta = np.max(x) - np.min(x)
-    #x = (x/delta + 0.5)
-    #return x.round() * 2 - 1
-    return x
+    delta = np.max(x) - np.min(x)
+    x = (x / delta + 0.5)
+    return x.round() * 2 - 1
 
 
 def dsq_round_back(dy):
@@ -37,9 +36,10 @@ def dsq(x, bit=8, name=None):
     bit_range = 2**bit - 1
 
     u_param_attr = fluid.ParamAttr(
-        initializer=fluid.initializer.ConstantInitializer(value=10))
+        initializer=fluid.initializer.ConstantInitializer(value=(2**31 - 1)))
     l_param_attr = fluid.ParamAttr(
-        initializer=fluid.initializer.ConstantInitializer(value=-10))
+        initializer=fluid.initializer.ConstantInitializer(value=(-1) *
+                                                          (2**31 - 1)))
 
     alpha_param_attr = fluid.ParamAttr(
         initializer=fluid.initializer.ConstantInitializer(value=0.2))
@@ -51,25 +51,27 @@ def dsq(x, bit=8, name=None):
         attr=alpha_param_attr, shape=[1], dtype=dtype)
 
     upper = fluid.layers.create_global_var(
-        shape=[1], value=10, dtype='float32', persistable=True)
+        shape=[1], value=(2**31 - 1), dtype='float32', persistable=True)
     lower = fluid.layers.create_global_var(
-        shape=[1], value=-10, dtype='float32', persistable=True)
+        shape=[1], value=(-1) * (2**31 - 1), dtype='float32', persistable=True)
     fluid.layers.assign(upper * 0.9 + u_param * 0.1, upper)
     fluid.layers.assign(lower * 0.9 + l_param * 0.1, lower)
     x = clip(x, upper, lower)
-    delta = (upper - lower) / bit_range
-    interval = (x - lower) // delta
-    mi = (interval + 0.5) * delta + lower
+    cur_max = fluid.layers.reduce_max(x)
+    cur_min = fluid.layers.reduce_min(x)
+    delta = (cur_max - cur_min) / bit_range
+    interval = (x - cur_min) // delta
+    mi = (interval + 0.5) * delta + cur_min
     x = phi_function(x, mi, alpha_param, delta)
     out_var = fluid.default_main_program().current_block().create_var(
-        name=x.name + '_dsq', dtype=dtype, shape=x.shape)
+        dtype=dtype, shape=x.shape)
     fluid.layers.py_func(
         func=dsq_round,
         x=x,
         out=out_var,
         backward_func=dsq_round_back,
         skip_vars_in_backward_input=[x, out_var])
-    x = dequantize(out_var, lower, delta, interval)
+    x = dequantize(out_var, cur_min, delta, interval)
 
     return x
 
