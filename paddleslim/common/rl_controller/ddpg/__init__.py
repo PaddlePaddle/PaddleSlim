689bddf4ab2104da4bb80402b50f594ba66ace92
ceci3 <ceci3@users.noreply.github.com>
2020-05-06 21:15:43 +0800

    Fix lstm windows (#255)
    * fix
    
    * fix win

--- paddleslim/common/rl_controller/ddpg/__init__.py
+++ paddleslim/common/rl_controller/ddpg/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020  PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"
 # you may not use this file except in compliance with the License.
@@ -12,16 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-from ..log_helper import get_logger
-_logger = get_logger(__name__, level=logging.INFO)
-try:
-    import parl
-    from .DDPG import *
-except ImportError as e:
-    _logger.warn(
-        "If you want to use DDPG in RLNAS, please pip intall parl first. Now states: {}".
-        format(e))
-
-from .LSTM import *
-from .utils import *
+from .ddpg_controller import *
