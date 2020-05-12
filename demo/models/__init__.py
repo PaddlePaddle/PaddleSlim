df8446a0a6b8ca99de5e18020ca1df6368ff0778
baiyfbupt <me@ethanbai.com>
2020-05-12 19:35:36 +0800

    add ResNet101_vd and MobileNetV2_x0_25 in demo/models

--- demo/models/__init__.py
+++ demo/models/__init__.py
@@ -1,13 +1,14 @@
 from __future__ import absolute_import
 from .mobilenet import MobileNet
 from .resnet import ResNet34, ResNet50
-from .resnet_vd import ResNet50_vd
-from .mobilenet_v2 import MobileNetV2
+from .resnet_vd import ResNet50_vd, ResNet101_vd
+from .mobilenet_v2 import MobileNetV2_x0_25, MobileNetV2
 from .pvanet import PVANet
 __all__ = [
     "model_list", "MobileNet", "ResNet34", "ResNet50", "MobileNetV2", "PVANet",
-    "ResNet50_vd"
+    "ResNet50_vd", "ResNet101_vd", "MobileNetV2_x0_25"
 ]
 model_list = [
-    'MobileNet', 'ResNet34', 'ResNet50', 'MobileNetV2', 'PVANet', 'ResNet50_vd'
+    'MobileNet', 'ResNet34', 'ResNet50', 'MobileNetV2', 'PVANet',
+    'ResNet50_vd', "ResNet101_vd", "MobileNetV2_x0_25"
 ]
