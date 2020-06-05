9763d6f2b9c6add93bdd23761ecf514a49865c72
slf12 <slf12thuss@163.com>
2020-06-05 15:32:10 +0000

    add pact demo

--- demo/models/__init__.py
+++ demo/models/__init__.py
@@ -5,6 +5,7 @@ from .resnet_vd import ResNet50_vd, ResNet101_vd
 from .mobilenet_v2 import MobileNetV2_x0_25, MobileNetV2
 from .pvanet import PVANet
 from .slimfacenet import SlimFaceNet_A_x0_60, SlimFaceNet_B_x0_75, SlimFaceNet_C_x0_75
+from .mobilenet_v3 import *
 __all__ = [
     "model_list", "MobileNet", "ResNet34", "ResNet50", "MobileNetV2", "PVANet",
     "ResNet50_vd", "ResNet101_vd", "MobileNetV2_x0_25"
@@ -13,3 +14,6 @@ model_list = [
     'MobileNet', 'ResNet34', 'ResNet50', 'MobileNetV2', 'PVANet',
     'ResNet50_vd', "ResNet101_vd", "MobileNetV2_x0_25"
 ]
+
+__all__ += mobilenet_v3.__all__
+model_list += mobilenet_v3.__all__
