b35729c33d9545712349d5cee56f68a8324639a7
yukavio <kavioyu@gmail.com>
2020-09-03 07:57:34 +0000

    add some layer to prune_walker test

--- tests/test_prune_walker.py
+++ tests/test_prune_walker.py
@@ -14,6 +14,7 @@
 import sys
 sys.path.append("../")
 import unittest
+import numpy as np
 import paddle.fluid as fluid
 from paddleslim.prune import Pruner
 from layers import conv_bn_layer
@@ -32,6 +33,7 @@ class TestPrune(unittest.TestCase):
         # O: prune input channels
         with fluid.program_guard(main_program, startup_program):
             input = fluid.data(name="image", shape=[None, 3, 16, 16])
+            label = fluid.data(name='label', shape=[None, 1], dtype='int64')
             conv1 = conv_bn_layer(input, 8, 3, "conv1", act='relu')
             conv2 = conv_bn_layer(conv1, 8, 3, "conv2", act='leaky_relu')
             sum1 = conv1 + conv2
@@ -43,10 +45,18 @@ class TestPrune(unittest.TestCase):
             conv6 = conv_bn_layer(sum3, 8, 3, "conv6")
             sub1 = conv6 - sum3
             mult = sub1 * sub1
-            conv7 = conv_bn_layer(mult, 8, 3, "Depthwise_Conv7", groups=8)
+            conv7 = conv_bn_layer(
+                mult, 8, 3, "Depthwise_Conv7", groups=8, use_cudnn=False)
             floored = fluid.layers.floor(conv7)
             scaled = fluid.layers.scale(floored)
             concated = fluid.layers.concat([scaled, mult], axis=1)
+            conv8 = conv_bn_layer(concated, 8, 3, "conv8")
+            feature = fluid.layers.reshape(conv8, [-1, 128, 16])
+            predict = fluid.layers.fc(input=feature, size=10, act='softmax')
+            cost = fluid.layers.cross_entropy(input=predict, label=label)
+            adam_optimizer = fluid.optimizer.AdamOptimizer(0.01)
+            avg_cost = fluid.layers.mean(cost)
+            adam_optimizer.minimize(avg_cost)
 
         params = []
         for param in main_program.all_parameters():
@@ -56,6 +66,12 @@ class TestPrune(unittest.TestCase):
         place = fluid.CPUPlace()
         exe = fluid.Executor(place)
         exe.run(startup_program)
+        x = np.random.random(size=(10, 3, 16, 16)).astype('float32')
+        label = np.random.random(size=(10, 1)).astype('int64')
+        loss_data, = exe.run(main_program,
+                             feed={"image": x,
+                                   "label": label},
+                             fetch_list=[cost.name])
         pruner = Pruner()
         main_program, _, _ = pruner.prune(
             main_program,
