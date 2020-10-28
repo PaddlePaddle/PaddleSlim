9b03e54f452ed12809a73de1ebf10bf6fb245113
baiyfbupt <me@ethanbai.com>
2020-10-28 14:13:54 +0800

    fix unittest

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -14,6 +14,7 @@
 import sys
 sys.path.append("../")
 import unittest
+import paddle
 from paddleslim.dist import merge, loss
 from layers import conv_bn_layer
 from static_case import StaticCase
--- tests/test_soft_label_loss.py
+++ tests/test_soft_label_loss.py
@@ -14,6 +14,7 @@
 import sys
 sys.path.append("../")
 import unittest
+import paddle
 from paddleslim.dist import merge, soft_label_loss
 from layers import conv_bn_layer
 from static_case import StaticCase
