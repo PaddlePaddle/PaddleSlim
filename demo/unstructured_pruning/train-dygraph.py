52ca3a33ca921abc02ea1e7137687894a5cdc0b7
liminghao <liminghao03@baidu.com>
2021-03-31 06:17:00 +0000

    unstructure pruning in dynamic graph, test=develop

---
+++ demo/unstructured_pruning/train-dygraph.py
@@ -0,0 +1,223 @@
+import paddle
+import os
+import sys
+import argparse
+import numpy as np
+from unstructure_pruner_dygraph import UnstructurePruner
+sys.path[0] = os.path.join(os.path.dirname("__file__"), os.path.pardir)
+from utility import add_arguments, print_arguments
+import models
+import paddle.vision.transforms as T
+import paddle.nn.functional as F
+import functools
+from paddle.vision.models import mobilenet_v1
+import time
+import logging
+from paddleslim.common import get_logger
+
+_logger = get_logger(__name__, level=logging.INFO)
+
+parser = argparse.ArgumentParser(description=__doc__)
+add_arg = functools.partial(add_arguments, argparser=parser)
+# yapf: disable
+add_arg('batch_size',       int,  64 * 4,                 "Minibatch size.")
+add_arg('use_gpu',          bool, True,                "Whether to use GPU or not.")
+add_arg('model',            str,  "MobileNet",                "The target model.")
+add_arg('pretrained_model', str,  "../pretrained_model/MobileNetV1_pretrained",                "Whether to use pretrained model.")
+add_arg('lr',               float,  0.1,               "The learning rate used to fine-tune pruned model.")
+add_arg('lr_strategy',      str,  "piecewise_decay",   "The learning rate decay strategy.")
+add_arg('l2_decay',         float,  3e-5,               "The l2_decay parameter.")
+add_arg('momentum_rate',    float,  0.9,               "The value of momentum_rate.")
+add_arg('ratio',            float,  0.3,               "The ratio to set zeros, the smaller part bounded by the ratio will be zeros.")
+add_arg('pruning_mode',            str,  'ratio',               "the pruning mode: whether by ratio or by threshold.")
+add_arg('threshold',            float,  0.001,               "The threshold to set zeros.")
+add_arg('num_epochs',       int,  120,               "The number of total epochs.")
+parser.add_argument('--step_epochs', nargs='+', type=int, default=[30, 60, 90], help="piecewise decay step")
+add_arg('data',             str, "mnist",                 "Which data to use. 'mnist' or 'imagenet'.")
+add_arg('log_period',       int, 100,                 "Log period in batches.")
+add_arg('phase',            str, "train",              "Whether to train or test the pruned model.")
+add_arg('test_period',      int, 1,                 "Test period in epoches.")
+add_arg('model_path',       str, "./models",         "The path to save model.")
+add_arg('model_period',     int, 10,             "The period to save model in epochs.")
+# yapf: enable
+
+model_list = models.__all__
+
+
+def piecewise_decay(args, step_per_epoch, model):
+    bd = [step_per_epoch * e for e in args.step_epochs]
+    lr = [args.lr * (0.1**i) for i in range(len(bd) + 1)]
+    learning_rate = paddle.optimizer.lr.PiecewiseDecay(boundaries=bd, values=lr)
+
+    optimizer = paddle.optimizer.Momentum(
+        learning_rate=learning_rate,
+        momentum=args.momentum_rate,
+        weight_decay=paddle.regularizer.L2Decay(args.l2_decay),
+        parameters=model.parameters())
+    return optimizer, learning_rate
+
+
+def cosine_decay(args, step_per_epoch, model):
+    learning_rate = paddle.optimizer.lr.CosineAnnealingDecay(
+        learning_rate=args.lr, T_max=args.num_epochs * step_per_epoch)
+    optimizer = paddle.optimizer.Momentum(
+        learning_rate=learning_rate,
+        momentum=args.momentum_rate,
+        weight_decay=paddle.regularizer.L2Decay(args.l2_decay),
+        parameters=model.parameters())
+    return optimizer, learning_rate
+
+
+def create_optimizer(args, step_per_epoch, model):
+    if args.lr_strategy == "piecewise_decay":
+        return piecewise_decay(args, step_per_epoch, model)
+    elif args.lr_strategy == "cosine_decay":
+        return cosine_decay(args, step_per_epoch, model)
+
+
+def compress(args):
+    train_reader = None
+    test_reader = None
+    if args.data == "mnist":
+        transform = T.Compose([T.Transpose(), T.Normalize([127.5], [127.5])])
+        train_dataset = paddle.vision.datasets.MNIST(
+            mode='train', backend="cv2", transform=transform)
+        val_dataset = paddle.vision.datasets.MNIST(
+            mode='test', backend="cv2", transform=transform)
+        class_dim = 10
+    elif args.data == "imagenet":
+        import imagenet_reader as reader
+        train_dataset = reader.ImageNetDataset(data_dir='/data', mode='train')
+        val_dataset = reader.ImageNetDataset(data_dir='/data', mode='val')
+        class_dim = 1000
+    elif args.data == "cifar10":
+        normalize = T.Normalize(
+            mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], data_format='CHW')
+        transform = T.Compose([T.Transpose(), normalize])
+        train_dataset = paddle.vision.datasets.Cifar10(
+            mode='train', backend='cv2', transform=transform)
+        val_dataset = paddle.vision.datasets.Cifar10(
+            mode='test', backend='cv2', transform=transform)
+        class_dim = 10
+    else:
+        raise ValueError("{} is not supported.".format(args.data))
+
+    places = paddle.static.cuda_places(
+    ) if args.use_gpu else paddle.static.cpu_places()
+    batch_size_per_card = int(args.batch_size / len(places))
+
+    train_loader = paddle.io.DataLoader(
+        train_dataset,
+        places=places,
+        drop_last=True,
+        batch_size=batch_size_per_card,
+        shuffle=True,
+        return_list=True,
+        num_workers=32)
+    valid_loader = paddle.io.DataLoader(
+        val_dataset,
+        places=places,
+        drop_last=False,
+        return_list=True,
+        batch_size=batch_size_per_card,
+        shuffle=False)
+    step_per_epoch = int(np.ceil(len(train_dataset) * 1. / args.batch_size))
+
+    # model definition
+    model = mobilenet_v1(num_classes=class_dim, pretrained=True)
+
+    opt, learning_rate = create_optimizer(args, step_per_epoch, model)
+
+    def test(epoch):
+        model.eval()
+        _logger.info(pruner.total_sparse())
+        acc_top1_ns = []
+        acc_top5_ns = []
+        for batch_id, data in enumerate(valid_loader):
+            start_time = time.time()
+            x_data = data[0]
+            y_data = paddle.to_tensor(data[1])
+            # y_data = paddle.unsqueeze(y_data, 1)
+            end_time = time.time()
+
+            logits = model(x_data)
+            loss = F.cross_entropy(logits, y_data)
+            acc_top1 = paddle.metric.accuracy(logits, y_data, k=1)
+            acc_top5 = paddle.metric.accuracy(logits, y_data, k=5)
+
+            acc_top1_ns.append(acc_top1.numpy())
+            acc_top5_ns.append(acc_top5.numpy())
+            if batch_id % args.log_period == 0:
+                _logger.info(
+                    "Eval epoch[{}] batch[{}] - acc_top1: {}; acc_top5: {}; time: {}".
+                    format(epoch, batch_id,
+                           np.mean(acc_top1.numpy()),
+                           np.mean(acc_top5.numpy()), end_time - start_time))
+            acc_top1_ns.append(np.mean(acc_top1.numpy()))
+            acc_top5_ns.append(np.mean(acc_top5.numpy()))
+
+        _logger.info("Final eval epoch[{}] - acc_top1: {}; acc_top5: {}".format(
+            epoch,
+            np.mean(np.array(acc_top1_ns)), np.mean(np.array(acc_top5_ns))))
+
+    def train(epoch):
+        model.train()
+        for batch_id, data in enumerate(train_loader):
+            start_time = time.time()
+            x_data = data[0]
+            y_data = paddle.to_tensor(data[1])
+            # y_data = paddle.unsqueeze(y_data, 1)
+
+            logits = model(x_data)
+            loss = F.cross_entropy(logits, y_data)
+            acc_top1 = paddle.metric.accuracy(logits, y_data, k=1)
+            acc_top5 = paddle.metric.accuracy(logits, y_data, k=5)
+            end_time = time.time()
+            if batch_id % args.log_period == 0:
+                _logger.info(
+                    "epoch[{}]-batch[{}] lr: {:.6f} - loss: {}; acc_top1: {}; acc_top5: {}; time: {}".
+                    format(epoch, batch_id, args.lr,
+                           np.mean(loss.numpy()),
+                           np.mean(acc_top1.numpy()),
+                           np.mean(acc_top5.numpy()), end_time - start_time))
+            loss.backward()
+            opt.step()
+            opt.clear_grad()
+            pruner.step()
+
+    if args.phase == 'pretrain':
+        pruner = UnstructurePruner(model, mode='ratio', ratio=0.0)
+        for i in range(args.resume_epoch, args.num_epochs):
+            train(i)
+            if i % args.test_period == 0: test(i)
+            if i % args.model_period == 0:
+                paddle.save(model.state_dict(), "dymodels/model.pdparams")
+                paddle.save(opt.state_dict(), "dymodels/opt.pdopt")
+    elif args.phase == 'prune':
+        pruner = UnstructurePruner(model, mode=args.pruning_mode)
+        test(0)
+        for i in range(args.resume_epoch, args.num_epochs):
+            train(i)
+            if i % args.test_period == 0:
+                pruner.update_params()
+                test(i)
+            if i % args.model_period == 0:
+                pruner.update_params()
+                paddle.save(model.state_dict(),
+                            "dymodels/model-pruned.pdparams")
+                paddle.save(opt.state_dict(), "dymodels/opt-pruned.pdopt")
+    elif args.phase == 'test':
+        model.set_state_dict(paddle.load("dymodels/model-pruned.pdparams"))
+        opt.set_state_dict(paddle.load("dymodels/opt-pruned.pdopt"))
+        test(0)
+
+
+def main():
+    paddle.disable_static()
+    args = parser.parse_args()
+    print_arguments(args)
+    compress(args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ demo/unstructured_pruning/unstructure_pruner_dygraph.py
@@ -0,0 +1,127 @@
+import numpy as np
+import paddle
+import logging
+from paddleslim.common import get_logger
+
+_logger = get_logger(__name__, level=logging.INFO)
+
+
+class UnstructurePruner():
+    """
+    The unstructure pruner.
+    Args:
+      - model(Paddle.Model): The model to be pruned.
+      - mode(ratio | threshold): Pruning mode, whether by the given ratio or threshold.
+      - threshold(float): The parameters whose absolute values are smaller than the THRESHOLD will be zeros. Default: None
+      - ratio(float): The parameters whose absolute values are in the smaller part decided by the ratio will be zeros. Default: None
+      - weights_keywords(List<String>): A list of string containing the keywords, meaning if this keyword is a substring of the parameter name, the parameter will be pruned. This argument could be used to ignore BN if you don't want to prune them. 
+    """
+
+    def __init__(self,
+                 model,
+                 mode,
+                 threshold=None,
+                 ratio=None,
+                 weights_keywords=['conv']):
+        assert mode in ('ratio', 'threshold'
+                        ), "mode must be selected from 'ratio' and 'threshold'"
+        if mode == 'ratio' and ratio is None:
+            _logger.info(
+                "The ratio mode is selected without setting the RATIO, default: 0.3"
+            )
+            ratio = 0.3
+            threshold = 0.0
+        if mode == 'threshold' and threshold is None:
+            _logger.info(
+                "The threshold mode is selected without setting the THRESHOLD, default: 0.01"
+            )
+            threshold = 0.01
+        self.model = model
+        self.mode = mode
+        self.threshold = threshold
+        self.ratio = ratio
+        self.weights_keywords = weights_keywords
+        self._apply_masks()
+
+    def mask_parameters(self, param, mask):
+        """
+        Update masks and parameters. It is executed to each layer before each iteration.
+        User can overwrite this function in subclass to implememt different pruning stragies.
+        Args:
+          - parameters(list<Tensor>): The parameters to be pruned.
+          - masks(list<Tensor>): The masks used to keep zero values in parameters.
+        """
+        bool_tmp = (paddle.abs(param) >= self.threshold)
+        paddle.assign(mask * bool_tmp, output=mask)
+        param_tmp = param * mask
+        param_tmp.stop_gradient = True
+        paddle.assign(param_tmp, output=param)
+
+    def _apply_masks(self):
+        self.masks = {}
+        for name, sub_layer in self.model.named_sublayers():
+            for param in sub_layer.parameters(include_sublayers=False):
+                tmp_array = np.ones(param.shape, dtype=np.float32)
+                mask_name = "_".join([param.name.replace(".", "_"), "mask"])
+                if mask_name not in sub_layer._buffers:
+                    sub_layer.register_buffer(mask_name,
+                                              paddle.to_tensor(tmp_array))
+                self.masks[param.name] = sub_layer._buffers[mask_name]
+        for name, sub_layer in self.model.named_sublayers():
+            sub_layer.register_forward_pre_hook(self._forward_pre_hook)
+
+    def _update_threshold(self):
+        params_flatten = []
+        for name, sub_layer in self.model.named_sublayers():
+            for param in sub_layer.parameters(include_sublayers=False):
+                if not 'conv' in param.name: continue
+                t_param = param.value().get_tensor()
+                v_param = np.array(t_param)
+                params_flatten.append(v_param.flatten())
+        params_flatten = np.concatenate(params_flatten, axis=0)
+        total_length = params_flatten.size
+        self.threshold = np.sort(np.abs(params_flatten))[max(
+            0, round(self.ratio * total_length) - 1)].item()
+
+    def step(self):
+        """
+        Update the threshold after each optimization step.
+        """
+        if self.mode == 'ratio':
+            self._update_threshold()
+        elif self.mode == 'threshold':
+            return
+
+    def _forward_pre_hook(self, layer, input):
+        for param in layer.parameters(include_sublayers=False):
+            mask = self.masks.get(param.name)
+            for word in self.weights_keywords:
+                if word in param.name:
+                    self.mask_parameters(param, mask)
+                    return input
+        return input
+
+    def update_params(self):
+        """
+        Update the parameters given self.masks, usually called before saving models and evaluation step during training. 
+        If you load a sparse model and only want to inference, no need to call the method.
+        """
+        for name, sub_layer in self.model.named_sublayers():
+            for param in sub_layer.parameters(include_sublayers=False):
+                mask = self.masks.get(param.name)
+                param_tmp = param * mask
+                param_tmp.stop_gradient = True
+                paddle.assign(param_tmp, output=param)
+
+    def total_sparse(self):
+        """
+        The function is used to get the whole model's density (1-sparsity).
+        """
+        total = 0
+        values = 0
+        for name, sub_layer in self.model.named_sublayers():
+            for param in sub_layer.parameters(include_sublayers=False):
+                total += np.product(param.shape)
+                values += len(paddle.nonzero(param))
+        ratio = float(values) / total
+        return ratio
