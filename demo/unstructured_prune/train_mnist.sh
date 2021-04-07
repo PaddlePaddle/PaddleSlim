2382a5ab82854de111f429cca5911d16ed72c3e1
minghaoBD <liminghao03@baidu.com>
2021-04-07 09:48:36 +0000

    add the API for structured pruning based on ratio and threshold modes in static and dynamic graph with demos, test=develop

---
+++ demo/unstructured_prune/train_mnist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+export CUDA_VISIBLE_DEVICES=2,3 
+export FLAGS_fraction_of_gpu_memory_to_use=0.98
+python3.7 train.py \
+          --batch_size=256 \
+          --data="mnist" \
+          --pruning_mode="threshold" \
+          --ratio=0.45 \
+          --threshold=1e-5 \
+          --lr=0.075 \
+          --pretrained_model="/PaddleSlim/demo/pretrained_model/MobileNetV1_pretrained"
