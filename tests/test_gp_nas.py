621d6e263806caa9cfd5a99d57c90acecc351f73
xiteng1988 <xiteng@bupt.edu.cn>
2021-04-20 20:21:40 +0800

    add gp_nas

--- tests/test_gp_nas.py
+++ tests/test_gp_nas.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+sys.path.append("../")
 import json
 import copy
 import unittest
