d06a9b47ab20bbb78e05081fde33e4d1990e85a9
ceci3 <592712189@qq.com>
2021-06-21 12:55:27 +0000

    update distill

--- paddleslim/dygraph/dist/distill.py
+++ paddleslim/dygraph/dist/distill.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
+import numpy as np
+import collections
 from collections import namedtuple
-import paddle
 import paddle.nn as nn
 from .losses import *
 
@@ -37,10 +38,9 @@ def add_distill_hook(net, outs, mapping_layers_name, layers_type):
     ### TODO: support DP model
     for idx, (n, m) in enumerate(net.named_sublayers()):
         if n in mapping_layers_name:
-            print(n, mapping_layers_name)
             midx = mapping_layers_name.index(n)
             m.register_forward_post_hook(
-                _get_activation(outs, layers_type[midx] + '_' + str(idx)))
+                _get_activation(outs, layers_type[midx]))
 
 
 DistillConfig = namedtuple(
@@ -52,34 +52,24 @@ DistillConfig = namedtuple(
 DistillConfig.__new__.__defaults__ = (None, ) * len(DistillConfig._fields)
 
 
-class Config:
-    def __init__(self,
-                 layer_S,
-                 layer_T,
-                 feature_type,
-                 loss_function,
-                 weight=1.0,
-                 align=None):
-        self.layer_S = layer_S
-        self.layer_T = layer_T
-        self.feature_type = feature_type
-        if loss_function in ['l1', 'l2', 'smooth_l1']:
-            self.loss_function = 'DistillationDistanceLoss'
-        elif loss_function in ['dml']:
-            self.loss_function = 'DistillationDMLLoss'
-        elif loss_function in ['rkl']:
-            self.loss_function = 'DistillationRKDLoss'
-        else:
-            raise NotImplementedError("loss function is not support!!!")
-        self.weight = weight
-        self.align = align
+def transpose_config(config):
+    assert 's_feature_idx' in config
+    assert 't_feature_idx' in config
+    assert 'feature_type' in config
+    assert 'loss_function' in config
 
-    @classmethod
-    def from_dict(cls, obj):
-        if obj is None:
-            return None
-        else:
-            return cls(**obj)
+    if config['loss_function'] in ['l1', 'l2', 'smooth_l1']:
+        config['loss_function'] = 'DistillationDistanceLoss'
+    elif config['loss_function'] in ['dml']:
+        config['loss_function'] = 'DistillationDMLLoss'
+    elif config['loss_function'] in ['rkl']:
+        config['loss_function'] = 'DistillationRKDLoss'
+    else:
+        raise NotImplementedError("loss function is not support!!!")
+    config['weight'] = config['weight'] if 'weight' in config else 1
+    ### TODO: add align in loss
+    config['align'] = config['align'] if 'align' in config else False
+    return config
 
 
 class Distill(nn.Layer):
@@ -95,23 +85,75 @@ class Distill(nn.Layer):
 
         self.stu_outs_dict, self.tea_outs_dict = self.prepare_outputs()
 
-        self.configs = [Config.from_dict(c) for c in self._distill_configs]
+        self.configs = []
+        for c in self._distill_configs:
+            self.configs.append(transpose_config(c))
+
+        self.get_distill_idx()
         self._loss_config_list = []
-        loss_config = {}
         for c in self.configs:
-            loss_config[str(c.loss_function)] = {}
-            cd = c.__dict__
-            loss_config[str(c.loss_function)]['weight'] = cd['weight']
-            loss_config[str(c.loss_function)]['key'] = cd['feature_type']
-            loss_config[str(c.loss_function)]['model_name_pairs'] = cd[
-                'feature_type']
-        self._loss_config_list.append(loss_config)
+            loss_config = {}
+            loss_config[str(c['loss_function'])] = {}
+            loss_config[str(c['loss_function'])]['weight'] = c['weight']
+            loss_config[str(c['loss_function'])]['key'] = c[
+                'feature_type'] + '_' + str(c['s_feature_idx']) + '_' + str(c[
+                    't_feature_idx'])
+            ### TODO: support list of student models and teacher_models
+            loss_config[str(c['loss_function'])][
+                'model_name_pairs'] = [['student', 'teacher']]
+            self._loss_config_list.append(loss_config)
         self.prepare_loss()
 
+    def get_distill_idx(self):
+        self.distill_idx = {}
+        for config in self._distill_configs:
+            if config['feature_type'] not in self.distill_idx:
+                self.distill_idx[config['feature_type']] = [[
+                    int(config['s_feature_idx']), int(config['t_feature_idx'])
+                ]]
+            else:
+                self.distill_idx[config['feature_type']].append([
+                    int(config['s_feature_idx']), int(config['t_feature_idx'])
+                ])
+
     def prepare_loss(self):
-        CombinedLoss(self._loss_config_list)
+        self.distill_loss = CombinedLoss(self._loss_config_list)
 
     def prepare_outputs(self):
-        stu_outs = self._adaptors_S(self._student_models)
-        tea_outs = self._adaptors_T(self._teacher_models)
-        return stu_outs, tea_outs
+        stu_outs_dict = collections.OrderedDict()
+        tea_outs_dict = collections.OrderedDict()
+        stu_outs_dict = self._adaptors_S(self._student_models, stu_outs_dict)
+        tea_outs_dict = self._adaptors_T(self._teacher_models, tea_outs_dict)
+        return stu_outs_dict, tea_outs_dict
+
+    def post_outputs(self):
+        final_keys = []
+        for key, value in self.stu_outs_dict.items():
+            if len(key.split('_')) == 1:
+                final_keys.append(key)
+
+        ### TODO: support list of student models and teacher_models
+        final_distill_dict = {
+            "student": collections.OrderedDict(),
+            "teacher": collections.OrderedDict()
+        }
+
+        for feature_type, dist_idx in self.distill_idx.items():
+            for idx, idx_list in enumerate(dist_idx):
+                sidx, tidx = idx_list[0], idx_list[1]
+                final_distill_dict['student'][feature_type + '_' + str(
+                    sidx) + '_' + str(tidx)] = self.stu_outs_dict[
+                        feature_type + '_' + str(sidx)]
+                final_distill_dict['teacher'][feature_type + '_' + str(
+                    sidx) + '_' + str(tidx)] = self.tea_outs_dict[
+                        feature_type + '_' + str(tidx)]
+        return final_distill_dict
+
+    def forward(self, *inputs, **kwargs):
+        stu_batch_outs = self._student_models.forward(*inputs, **kwargs)
+        tea_batch_outs = self._teacher_models.forward(*inputs, **kwargs)
+        distill_inputs = self.post_outputs()
+        ### batch is None just for now
+        distill_outputs = self.distill_loss(distill_inputs, None)
+        distill_loss = distill_outputs['loss']
+        return stu_batch_outs, tea_batch_outs, distill_loss
--- tests/dygraph/test_distill.py
+++ tests/dygraph/test_distill.py
@@ -1,65 +1,112 @@
 import sys
 sys.path.append("../../")
+import logging
 import numpy as np
 import unittest
 import paddle
+from paddle.fluid.log_helper import get_logger
 from paddle.vision.models import MobileNetV1
 import paddle.vision.transforms as T
 from paddleslim.dygraph.dist import Distill, DistillConfig, add_distill_hook
 
-s_model = MobileNetV1()
-t_model = MobileNetV1()
+_logger = get_logger(
+    __name__, logging.INFO, fmt='%(asctime)s-%(levelname)s: %(message)s')
 
 
-def adaptor(model):
-    mapping_keys = ['hidden', 'logits']
-    mapping_layers = dict.fromkeys(mapping_keys, [])
-    add_distill_hook(model, mapping_layers, ['conv1'], ['hidden'])
-    add_distill_hook(model, mapping_layers, ['conv2_2'], ['hidden'])
-    add_distill_hook(model, mapping_layers, ['conv3_2', 'conv4_2'],
-                     ['hidden', 'hidden'])
-    add_distill_hook(model, mapping_layers, ['fc'], ['logits'])
-    return mapping_layers
+class TestImperativeDistill(unittest.TestCase):
+    def setUp(self):
+        self.s_model = MobileNetV1()
+        self.t_model = MobileNetV1()
+        self.t_model.eval()
+
+    def test_distill(self):
+        transform = T.Compose([T.Transpose(), T.Normalize([127.5], [127.5])])
+
+        train_dataset = paddle.vision.datasets.Cifar10(
+            mode='train', backend='cv2', transform=transform)
+        val_dataset = paddle.vision.datasets.Cifar10(
+            mode='test', backend='cv2', transform=transform)
+
+        place = paddle.CUDAPlace(0) if paddle.is_compiled_with_cuda(
+        ) else paddle.CPUPlace()
+        train_reader = paddle.io.DataLoader(
+            train_dataset, drop_last=True, places=place, batch_size=64)
+        test_reader = paddle.io.DataLoader(
+            val_dataset, places=place, batch_size=64)
+
+        def adaptor(model, mapping_layers):
+            mapping_keys = ['hidden', 'logits']
+            for k in mapping_keys:
+                mapping_layers[k] = []
+            add_distill_hook(model, mapping_layers, ['conv1'], ['hidden_0'])
+            add_distill_hook(model, mapping_layers, ['conv2_2'], ['hidden_1'])
+            add_distill_hook(model, mapping_layers, ['conv3_2', 'conv4_2'],
+                             ['hidden_2', 'hidden_3'])
+            add_distill_hook(model, mapping_layers, ['fc'], ['logits_0'])
+            return mapping_layers
+
+        def test(model):
+            model.eval()
+            avg_acc = [[], []]
+            for batch_id, data in enumerate(test_reader):
+                img = paddle.to_tensor(data[0])
+                label = paddle.to_tensor(data[1])
+                label = paddle.reshape(label, [-1, 1])
+                out = model(img)
+                acc_top1 = paddle.metric.accuracy(input=out, label=label, k=1)
+                acc_top5 = paddle.metric.accuracy(input=out, label=label, k=5)
+                avg_acc[0].append(acc_top1.numpy())
+                avg_acc[1].append(acc_top5.numpy())
+                if batch_id % 100 == 0:
+                    _logger.info(
+                        "Test | step {}: acc1 = {:}, acc5 = {:}".format(
+                            batch_id, acc_top1.numpy(), acc_top5.numpy()))
+
+            _logger.info("Test |Average: acc_top1 {}, acc_top5 {}".format(
+                np.mean(avg_acc[0]), np.mean(avg_acc[1])))
+
+        def train(model):
+            adam = paddle.optimizer.Adam(
+                learning_rate=0.001, parameters=distill_model.parameters())
+
+            for batch_id, data in enumerate(train_reader):
+                img = paddle.to_tensor(data[0])
+                label = paddle.to_tensor(data[1])
+                student_out, teacher_out, distill_loss = distill_model(img)
+                loss = paddle.nn.functional.loss.cross_entropy(student_out,
+                                                               label)
+                avg_loss = paddle.mean(loss)
+                all_loss = avg_loss + distill_loss
+                all_loss.backward()
+                adam.step()
+                adam.clear_grad()
+                if batch_id % 100 == 0:
+                    _logger.info("Train | At epoch {} step {}: loss = {:}".
+                                 format(str(0), batch_id, all_loss.numpy()))
+            test(self.s_model)
+            self.s_model.train()
+
+        distill_configs = [{
+            's_feature_idx': 0,
+            't_feature_idx': 0,
+            'feature_type': 'hidden',
+            'loss_function': 'l2'
+        }, {
+            's_feature_idx': 1,
+            't_feature_idx': 1,
+            'feature_type': 'hidden',
+            'loss_function': 'l2'
+        }, {
+            's_feature_idx': 0,
+            't_feature_idx': 0,
+            'feature_type': 'logits',
+            'loss_function': 'l2'
+        }]
+
+        distill_model = Distill(distill_configs, self.s_model, self.t_model,
+                                adaptor, adaptor)
+        train(distill_model)
 
 
-layer_configs = [{
-    'layer_S': 0,
-    'layer_T': 0,
-    'feature_type': 'hidden',
-    'loss_function': 'l2'
-}, {
-    'layer_S': 1,
-    'layer_T': 1,
-    'feature_type': 'hidden',
-    'loss_function': 'l2'
-}, {
-    'layer_S': 0,
-    'layer_T': 0,
-    'feature_type': 'logits',
-    'loss_function': 'l2'
-}]
-
-distill = Distill(layer_configs, s_model, s_model, adaptor, adaptor)
-
-transform = T.Compose([T.Transpose(), T.Normalize([127.5], [127.5])])
-
-train_dataset = paddle.vision.datasets.Cifar10(
-    mode='train', backend='cv2', transform=transform)
-val_dataset = paddle.vision.datasets.Cifar10(
-    mode='test', backend='cv2', transform=transform)
-
-place = paddle.CUDAPlace(0) if paddle.is_compiled_with_cuda(
-) else paddle.CPUPlace()
-train_reader = paddle.io.DataLoader(
-    train_dataset, drop_last=True, places=place, batch_size=64)
-test_reader = paddle.io.DataLoader(val_dataset, places=place, batch_size=64)
-
-adam = paddle.optimizer.Adam(
-    learning_rate=0.001, parameters=s_model.parameters() + distill.parameters())
-
-for batch_id, data in enumerate(train_reader):
-    img = paddle.to_tensor(data[0])
-    label = paddle.to_tensor(data[1])
-    out = s_model(img)
-    if batch_id == 2:
-        sys.exit(0)
+if __name__ == '__main__':
+    unittest.main()
