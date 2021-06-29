6c6d038b203fe7f9edfeb3fed4a6cb17f3cb212b
littletomatodonkey <dazhiningsibuqu@163.com>
2021-06-29 14:13:28 +0000

    fix

--- tests/dygraph/test_distillation_loss.py
+++ tests/dygraph/test_distillation_loss.py
@@ -17,8 +17,14 @@ sys.path.append("../../")
 import unittest
 import paddle
 
+# basic loss
 from paddleslim.dygraph.dist.losses import CombinedLoss
-from paddleslim.dygraph.dist.losses import L1Loss, L2Loss
+from paddleslim.dygraph.dist.losses import DistanceLoss, L2Loss
+
+# distillation loss
+from paddleslim.dygraph.dist.losses import DistillationDistanceLoss
+from paddleslim.dygraph.dist.losses import DistillationRKDLoss
+from paddleslim.dygraph.dist.losses import DistillationDMLLoss
 
 import numpy as np
 
@@ -45,6 +51,21 @@ class TestL2Loss(unittest.TestCase):
             out = np.sum(diff)
         return out
 
+    def dist_np_l2_loss(self,
+                        predicts,
+                        reduction="none",
+                        model_name_pairs=(["", ""]),
+                        key=None):
+        for idx, pair in enumerate(model_name_pairs):
+            out1 = predicts[pair[0]]
+            out2 = predicts[pair[1]]
+            if self.key is not None:
+                out1 = out1[key]
+                out2 = out2[key]
+            loss = self.np_l2_loss(out1, out2, reduction=self.key)
+            loss_dict["{}_{}_{}_{}".format(self.name, pair[0], pair[1],
+                                           idx)] = loss
+
     def test_basic_l2_loss(self):
         shape = [10, 20]
         x = paddle.rand(shape)
@@ -60,6 +81,30 @@ class TestL2Loss(unittest.TestCase):
                 loss_func = L2Loss(reduction=mode)
                 pd_result = loss_func(x, y).numpy()
                 self.assertTrue(np.allclose(np_result, pd_result))
+
+    def test_distillation_l2_loss_wo_key():
+        shape = [20, 10]
+        x_feat_name = "feat_x"
+        y_feat_name = "feat_y"
+        x_dict = {x_feat_name: paddle.rand(shape)}
+        y_dict = {y_feat_name: paddle.rand(shape)}
+
+        modes = ["none", "mean", "sum"]
+        devices = ["cpu"]
+        if paddle.is_compiled_with_cuda():
+            devices.append("gpu")
+        for device in devices:
+            paddle.set_device(device)
+            for mode in modes:
+                np_result = self.np_l2_loss(x, y, reduction=mode)
+                loss_func = L2Loss(reduction=mode)
+                pd_result = loss_func(x, y).numpy()
+                self.assertTrue(np.allclose(np_result, pd_result))
+
+        loss_func = DistillationDistanceLoss(
+            mode="l2", model_name_pairs=[["feat", "feat"]])
+
+        shape = single
 
 
 if __name__ == '__main__':
