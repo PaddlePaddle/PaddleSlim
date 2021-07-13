068119c9ad18fa07f354fdcad7cc5bb73d0724b3
cc <52520497+juncaipeng@users.noreply.github.com>
2021-07-13 19:28:37 +0800

    [Quant] add dygraph ptq and refine dygraph qat (#838)
    * add dygraph ptq and refine dygraph qat
    
    * add unit test for ptq
    
    * update according to comments

--- tests/dygraph/test_qat.py
+++ tests/dygraph/test_qat.py
@@ -66,20 +66,30 @@ class ImperativeLenet(nn.Layer):
         return x
 
 
-class TestImperativeQatDefaultConfig(unittest.TestCase):
+class TestQAT(unittest.TestCase):
     """
     QAT = quantization-aware training
     This test case uses defualt quantization config, weight_quantize_type 
     is channel_wise_abs_max
     """
 
-    def test_qat_acc(self):
-        lenet = ImperativeLenet()
-        quanter = QAT()
-        quanter.quantize(lenet)
+    def set_seed(self):
+        seed = 1
+        np.random.seed(seed)
+        paddle.static.default_main_program().random_seed = seed
+        paddle.static.default_startup_program().random_seed = seed
 
-        place = paddle.CUDAPlace(0) if paddle.is_compiled_with_cuda(
-        ) else paddle.CPUPlace()
+    def prepare(self):
+        self.quanter = QAT()
+
+    def test_qat_acc(self):
+        self.prepare()
+        self.set_seed()
+
+        fp32_lenet = ImperativeLenet()
+
+        place = paddle.CUDAPlace(0) \
+            if paddle.is_compiled_with_cuda() else paddle.CPUPlace()
 
         transform = T.Compose([T.Transpose(), T.Normalize([127.5], [127.5])])
 
@@ -141,130 +151,46 @@ class TestImperativeQatDefaultConfig(unittest.TestCase):
                         "Test | step {}: acc1 = {:}, acc5 = {:}".format(
                             batch_id, acc_top1.numpy(), acc_top5.numpy()))
 
-            _logger.info("Test |Average: acc_top1 {}, acc_top5 {}".format(
+            _logger.info("Test | Average: acc_top1 {}, acc_top5 {}".format(
                 np.mean(avg_acc[0]), np.mean(avg_acc[1])))
             return np.mean(avg_acc[0]), np.mean(avg_acc[1])
 
-        train(lenet)
-        top1_1, top5_1 = test(lenet)
+        train(fp32_lenet)
+        top1_1, top5_1 = test(fp32_lenet)
 
-        lenet.__init__()
-        train(lenet)
-        top1_2, top5_2 = test(lenet)
+        fp32_lenet.__init__()
+        quant_lenet = self.quanter.quantize(fp32_lenet)
+        train(quant_lenet)
+        top1_2, top5_2 = test(quant_lenet)
+        self.quanter.save_quantized_model(
+            quant_lenet,
+            './tmp/qat',
+            input_spec=[
+                paddle.static.InputSpec(
+                    shape=[None, 1, 28, 28], dtype='float32')
+            ])
 
         # values before quantization and after quantization should be close
-        _logger.info("Before quantization: top1: {}, top5: {}".format(top1_2,
-                                                                      top5_2))
-        _logger.info("After quantization: top1: {}, top5: {}".format(top1_1,
-                                                                     top5_1))
+        _logger.info("Before quantization: top1: {}, top5: {}".format(top1_1,
+                                                                      top5_1))
+        _logger.info("After quantization: top1: {}, top5: {}".format(top1_2,
+                                                                     top5_2))
+        _logger.info("\n")
+
+        diff = 0.002
+        self.assertTrue(
+            top1_1 - top1_2 < diff,
+            msg="The acc of quant model is too lower than fp32 model")
 
 
-class TestImperativeQatPACT(unittest.TestCase):
+class TestQATWithPACT(TestQAT):
     """
-    QAT = quantization-aware training
     This test case is for testing user defined quantization.
     """
 
-    def test_qat_acc(self):
-        lenet = ImperativeLenet()
-        quant_config = {
-            'activation_preprocess_type': 'PACT',
-            'quantizable_layer_type': ['Conv2D', 'Linear'],
-        }
-        quanter = QAT(config=quant_config)
-        quanter.quantize(lenet)
-
-        place = paddle.CUDAPlace(0) if paddle.is_compiled_with_cuda(
-        ) else paddle.CPUPlace()
-
-        transform = T.Compose([T.Transpose(), T.Normalize([127.5], [127.5])])
-
-        train_dataset = paddle.vision.datasets.MNIST(
-            mode='train', backend='cv2', transform=transform)
-        val_dataset = paddle.vision.datasets.MNIST(
-            mode='test', backend='cv2', transform=transform)
-        train_reader = paddle.io.DataLoader(
-            train_dataset, drop_last=True, places=place, batch_size=64)
-        test_reader = paddle.io.DataLoader(
-            val_dataset, places=place, batch_size=64)
-
-        def train(model):
-            adam = paddle.optimizer.Adam(
-                learning_rate=0.001, parameters=model.parameters())
-            epoch_num = 1
-            for epoch in range(epoch_num):
-                model.train()
-                for batch_id, data in enumerate(train_reader):
-                    img = paddle.to_tensor(data[0])
-                    label = paddle.to_tensor(data[1])
-                    img = paddle.reshape(img, [-1, 1, 28, 28])
-                    label = paddle.reshape(label, [-1, 1])
-
-                    out = model(img)
-                    acc = paddle.metric.accuracy(out, label)
-                    loss = paddle.nn.functional.loss.cross_entropy(out, label)
-                    avg_loss = paddle.mean(loss)
-                    avg_loss.backward()
-                    adam.minimize(avg_loss)
-                    model.clear_gradients()
-                    if batch_id % 100 == 0:
-                        _logger.info(
-                            "Train | At epoch {} step {}: loss = {:}, acc= {:}".
-                            format(epoch, batch_id,
-                                   avg_loss.numpy(), acc.numpy()))
-
-        def test(model):
-            model.eval()
-            avg_acc = [[], []]
-            for batch_id, data in enumerate(test_reader):
-                img = paddle.to_tensor(data[0])
-                label = paddle.to_tensor(data[1])
-                img = paddle.reshape(img, [-1, 1, 28, 28])
-                label = paddle.reshape(label, [-1, 1])
-
-                out = model(img)
-                acc_top1 = paddle.metric.accuracy(input=out, label=label, k=1)
-                acc_top5 = paddle.metric.accuracy(input=out, label=label, k=5)
-                avg_acc[0].append(acc_top1.numpy())
-                avg_acc[1].append(acc_top5.numpy())
-                if batch_id % 100 == 0:
-                    _logger.info(
-                        "Test | step {}: acc1 = {:}, acc5 = {:}".format(
-                            batch_id, acc_top1.numpy(), acc_top5.numpy()))
-
-            _logger.info("Test |Average: acc_top1 {}, acc_top5 {}".format(
-                np.mean(avg_acc[0]), np.mean(avg_acc[1])))
-            return np.mean(avg_acc[0]), np.mean(avg_acc[1])
-
-        train(lenet)
-        top1_1, top5_1 = test(lenet)
-        quanter.save_quantized_model(
-            lenet,
-            './dygraph_qat',
-            input_spec=[
-                paddle.static.InputSpec(
-                    shape=[None, 1, 28, 28], dtype='float32')
-            ])
-
-        lenet.__init__()
-        train(lenet)
-        top1_2, top5_2 = test(lenet)
-
-        # values before quantization and after quantization should be close
-        _logger.info("Before quantization: top1: {}, top5: {}".format(top1_2,
-                                                                      top5_2))
-        _logger.info("After quantization: top1: {}, top5: {}".format(top1_1,
-                                                                     top5_1))
-
-        # test for saving model in train mode
-        lenet.train()
-        quanter.save_quantized_model(
-            lenet,
-            './dygraph_qat',
-            input_spec=[
-                paddle.static.InputSpec(
-                    shape=[None, 1, 28, 28], dtype='float32')
-            ])
+    def prepare(self):
+        quant_config = {'activation_preprocess_type': 'PACT', }
+        self.quanter = QAT(config=quant_config)
 
 
 if __name__ == '__main__':
