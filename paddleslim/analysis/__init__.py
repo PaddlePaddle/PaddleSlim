f48cd7644da83d0f616e88b61ad891a72bfae70d
guozichao <guozichao@baidu.com>
2021-09-27 10:08:16 +0800

    init

--- paddleslim/analysis/__init__.py
+++ paddleslim/analysis/__init__.py
@@ -14,8 +14,11 @@
 from .flops import flops, dygraph_flops
 from .model_size import model_size
 from .latency import LatencyEvaluator, TableLatencyEvaluator
+from .LatencyPredictor import LatencyPredictor, TableLatencyPredictor
+from _utils import get_key_from_op, save_cls_model, save_det_model, save_seg_model
 
 __all__ = [
     'flops', 'dygraph_flops', 'model_size', 'LatencyEvaluator',
-    'TableLatencyEvaluator'
+    'TableLatencyEvaluator', "LatencyPredictor", "TableLatencyPredictor",
+    "get_key_from_op", "save_cls_model", "save_det_model", "save_seg_model",
 ]
