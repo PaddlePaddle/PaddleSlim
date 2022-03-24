ef927cb0fffe16bd0cf18cdc19c743b3ad213fa5
RachelXu7 <molixu7@gmail.com>
2022-03-24 12:47:52 +0000

    speedup_sparse_block

--- paddleslim/prune/unstructured_pruner_utils.py
+++ paddleslim/prune/unstructured_pruner_utils.py
@@ -8,11 +8,33 @@ BLOCK_SPARSE_ACCURATE_THRESHOLD = 0.05
 
 def cal_mxn_avg_matrix(mat, m=1, n=1):
     if m == 1 and n == 1: return copy.deepcopy(mat)
-    avg_mat = np.zeros_like(mat)
-    rows = len(mat) // m + 1
-    cols = len(mat[0]) // n + 1
-    for row in range(rows):
-        for col in range(cols):
-            avg_mat[m * row:m * row + m, n * col:n * col + n] = np.mean(mat[
-                m * row:m * row + m, n * col:n * col + n])
-    return avg_mat
+    assert m == n, "The block size m and n should be same, but got m={}, n={}".format(
+        m, n)
+    ori_row, ori_col = mat.shape[0], mat.shape[1]
+    print(mat.shape)
+    if len(mat.shape) == 4:
+        assert mat.shape[2:] == (1, 1), "Only support for (n, n, 1, 1) for now."
+        new_mat = mat.reshape(ori_row, ori_col)
+    else:
+        new_mat = np.array(mat)
+    res_col = m - len(mat[0]) % m
+    res_row = m - len(mat) % m
+
+    new_mat = np.pad(new_mat, ((0, res_col), (0, res_col)), 'reflect')
+    final_mat = np.zeros_like(new_mat)
+
+    new_shape = [len(new_mat) // m, len(new_mat[0]) // m, m, m]
+    strides = new_mat.itemsize * np.array(
+        [len(new_mat) * m, m, len(new_mat), 1])
+    new_mat = np.lib.stride_tricks.as_strided(
+        new_mat, shape=new_shape, strides=strides)
+    new_mat = new_mat.mean((2, 3), keepdims=True)
+    new_mat = np.tile(new_mat, (1, 1, m, m))
+    for i in range(len(new_mat)):
+        sub_array = new_mat[i]
+        tmp = np.concatenate(list(sub_array), axis=1)
+        final_mat[i * m:i * m + m] = tmp
+    final_mat = final_mat[:ori_row, :ori_col]
+    if len(mat.shape) == 4:
+        final_mat = final_mat.reshape(ori_row, ori_col, 1, 1)
+    return final_mat
