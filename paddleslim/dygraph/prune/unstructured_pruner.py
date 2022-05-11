a6259ba4fef52c02e4c28029e0e9bf4688710876
minghaoBD <liminghao03@baidu.com>
2022-05-11 14:07:17 +0000

    pre-release for fc-sparse

--- paddleslim/dygraph/prune/unstructured_pruner.py
+++ paddleslim/dygraph/prune/unstructured_pruner.py
@@ -65,6 +65,8 @@ class UnstructuredPruner():
             skip_params_func = skip_params_func
         elif prune_params_type == 'conv1x1_only':
             skip_params_func = self._get_skip_params_conv1x1
+        elif prune_params_type == 'fc':
+            skip_params_func = self._get_skip_params_fc
         elif skip_params_func is None:
             skip_params_func = self._get_skip_params
 
@@ -233,7 +235,7 @@ class UnstructuredPruner():
         return ratio
 
     @staticmethod
-    def total_sparse_conv1x1(model):
+    def total_sparse_conv1x1(model, debug_mode=False):
         """
         This static function is used to get the partial model's sparsity in terms of conv1x1 layers.
         It is static because during testing, we can calculate sparsity without initializing a pruner instance.
@@ -254,6 +256,9 @@ class UnstructuredPruner():
                 if not cond: continue
                 total += np.product(param.shape)
                 values += len(paddle.nonzero(param))
+                if not debug_mode: continue
+                den = float(paddle.nonzero(param)) / np.product(param.shape)
+                print(param.name, param.shape, 1 - den)
         ratio = 1 - float(values) / total
         return ratio
 
@@ -287,6 +292,18 @@ class UnstructuredPruner():
                 if len(param.shape) == 1: skip_params.add(param.name)
                 cond = len(param.shape) == 4 and param.shape[
                     2] == 1 and param.shape[3] == 1
+                if not cond: skip_params.add(param.name)
+        return skip_params
+
+    def _get_skip_params_fc(self, model):
+        skip_params = set()
+        for _, sub_layer in model.named_sublayers():
+            if type(sub_layer).__name__.split('.')[-1] in NORMS_ALL:
+                skip_params.add(sub_layer.full_name())
+            for param in sub_layer.parameters(include_sublayers=False):
+                # exclude bias whose shape is like (n,)
+                if len(param.shape) == 1: skip_params.add(param.name)
+                cond = len(param.shape) == 2
                 if not cond: skip_params.add(param.name)
         return skip_params
 
