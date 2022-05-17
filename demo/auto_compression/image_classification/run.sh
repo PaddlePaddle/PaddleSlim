637e165fb09b1f398987aedd571ee2dd2cc7caa2
Chang Xu <molixu7@gmail.com>
2022-05-17 13:48:02 +0800

    add ACT mobilenetv1 demo (#1109)

---
+++ demo/auto_compression/image_classification/run.sh
@@ -0,0 +1,20 @@
+# 单卡启动
+python run.py \
+    --model_dir='MobileNetV1_infer' \
+    --model_filename='inference.pdmodel' \
+    --params_filename='inference.pdiparams' \
+    --save_dir='./save_quant_mobilev1/' \
+    --batch_size=128 \
+    --config_path='./configs/mobilev1.yaml'\
+    --data_dir='ILSVRC2012'
+    
+# 多卡启动    
+# python -m paddle.distributed.launch run.py \
+#     --model_dir='MobileNetV1_infer' \
+#     --model_filename='inference.pdmodel' \
+#     --params_filename='inference.pdiparams' \
+#     --save_dir='./save_quant_mobilev1/' \
+#     --batch_size=128 \
+#     --config_path='./configs/mobilev1.yaml'\
+#     --data_dir='/workspace/dataset/ILSVRC2012/' 
+    
