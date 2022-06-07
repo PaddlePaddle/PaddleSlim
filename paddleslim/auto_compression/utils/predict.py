2952d4cef2d8ea27d6205f6f8fc8309238532983
whs <wanghaoshuang@baidu.com>
2022-06-07 17:16:26 +0800

    Skip auto tuning when shape of model's  input is variable (#1155)

--- paddleslim/auto_compression/utils/predict.py
+++ paddleslim/auto_compression/utils/predict.py
@@ -6,6 +6,30 @@ from .prune_model import get_sparse_model, get_prune_model
 from .fake_ptq import post_quant_fake
 
 
+def with_variable_shape(model_dir, model_filename=None, params_filename=None):
+    """
+    Whether the shape of model's input is variable.
+    Args:
+        path_prefix(str | None): Directory path to save model + model name without suffix.
+        model_filename(str): specify model_filename if you don't want to use default name. Default : 'None'.
+        params_filename(str): specify params_filename if you don't want to use default name. Default : 'None'.
+    Returns:
+        bool: Whether the shape of model's input is variable.
+    """
+    paddle.enable_static()
+    exe = paddle.static.Executor(paddle.CPUPlace())
+    [inference_program, feed_target_names, fetch_targets] = (
+        paddle.static.load_inference_model(
+            model_dir,
+            exe,
+            model_filename=model_filename,
+            params_filename=params_filename))
+    for var_ in inference_program.list_vars():
+        if var_.name in feed_target_names:
+            if var_.shape.count(-1) > 1:
+                return True
+
+
 def predict_compressed_model(executor,
                              places,
                              model_dir,
