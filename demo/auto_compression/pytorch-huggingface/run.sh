68060ea9faed6110279827ca0a1a6bad6ad063c7
Chang Xu <molixu7@gmail.com>
2022-06-27 16:20:03 +0800

    Update run.sh

--- demo/auto_compression/pytorch-huggingface/run.sh
+++ demo/auto_compression/pytorch-huggingface/run.sh
@@ -1,4 +1,1 @@
-export PYTHONPATH=/paddle/auto_compress/final_git/PaddleSlim/:$PYTHONPATH
-export PYTHONPATH=/paddle/auto_compress/PaddleNLP/:$PYTHONPATH
-
-python3.7 run.py --config_path=./configs/cola.yaml --save_dir='./output/cola/'
+python run.py --config_path=./configs/cola.yaml --save_dir='./output/cola/'
