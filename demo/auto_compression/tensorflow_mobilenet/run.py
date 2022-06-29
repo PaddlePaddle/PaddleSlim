f275cefa2d9825a597c43189f9068a25394ad303
ceci3 <ceci3@users.noreply.github.com>
2022-06-29 16:20:06 +0800

    add tf mbv1 demo (#1205)

--- demo/auto_compression/tensorflow_mobilenet/run.py
+++ demo/auto_compression/tensorflow_mobilenet/run.py
@@ -14,18 +14,15 @@
 
 import os
 import sys
-sys.path[0] = os.path.join(
-    os.path.dirname("__file__"), os.path.pardir, os.path.pardir)
 import argparse
 import functools
 from functools import partial
 
 import numpy as np
-import math
 import paddle
 import paddle.nn as nn
-from paddle.io import Dataset, BatchSampler, DataLoader
-import imagenet_reader as reader
+from paddle.io import DataLoader
+from imagenet_reader import ImageNetDataset
 from paddleslim.auto_compression.config_helpers import load_config as load_slim_config
 from paddleslim.auto_compression import AutoCompression
 
@@ -43,46 +40,38 @@ def argsparser():
         type=str,
         default='output',
         help="directory to save compressed model.")
-    parser.add_argument(
-        '--total_images',
-        type=int,
-        default=1281167,
-        help="the number of total training images.")
     return parser
 
 
 # yapf: enable
 def reader_wrapper(reader, input_name):
     def gen():
-        for i, data in enumerate(reader()):
-            imgs = np.float32([item[0] for item in data])
+        for i, (imgs, label) in enumerate(reader()):
             yield {input_name: imgs}
 
     return gen
 
 
 def eval_reader(data_dir, batch_size):
-    val_reader = paddle.batch(
-        reader.val(data_dir=data_dir), batch_size=batch_size)
-    return val_reader
+    val_reader = ImageNetDataset(mode='val', data_dir=data_dir)
+    val_loader = DataLoader(
+        val_reader,
+        batch_size=global_config['batch_size'],
+        shuffle=False,
+        drop_last=False,
+        num_workers=0)
+    return val_loader
 
 
 def eval_function(exe, compiled_test_program, test_feed_names, test_fetch_list):
-    val_reader = eval_reader(data_dir, batch_size=global_config['batch_size'])
-    image = paddle.static.data(
-        name=global_config['input_name'],
-        shape=[None, 3, 224, 224],
-        dtype='float32')
-    label = paddle.static.data(name='label', shape=[None, 1], dtype='int64')
+    val_loader = eval_reader(data_dir, batch_size=global_config['batch_size'])
 
     results = []
-    print('Evaluating... It will take a while. Please wait...')
-    for batch_id, data in enumerate(val_reader()):
+    for batch_id, (image, label) in enumerate(val_loader):
         # top1_acc, top5_acc
         if len(test_feed_names) == 1:
-            image = np.array([[d[0]] for d in data])
-            image = image.reshape((len(data), 3, 224, 224))
-            label = [[d[1]] for d in data]
+            image = np.array(image)
+            label = np.array(label).astype('int64')
             pred = exe.run(compiled_test_program,
                            feed={test_feed_names[0]: image},
                            fetch_list=test_fetch_list)
@@ -100,9 +89,8 @@ def eval_function(exe, compiled_test_program, test_feed_names, test_fetch_list):
             results.append([top_1, top_5])
         else:
             # eval "eval model", which inputs are image and label, output is top1 and top5 accuracy
-            image = np.array([[d[0]] for d in data])
-            image = image.reshape((len(data), 3, 224, 224))
-            label = [[d[1]] for d in data]
+            image = np.array(image)
+            label = np.array(label).astype('int64')
             result = exe.run(
                 compiled_test_program,
                 feed={test_feed_names[0]: image,
@@ -110,6 +98,8 @@ def eval_function(exe, compiled_test_program, test_feed_names, test_fetch_list):
                 fetch_list=test_fetch_list)
             result = [np.mean(r) for r in result]
             results.append(result)
+        if batch_id % 50 == 0:
+            print('Eval iter: ', batch_id)
     result = np.mean(np.array(results), axis=0)
     return result[0]
 
@@ -119,21 +109,18 @@ def main():
     all_config = load_slim_config(args.config_path)
     assert "Global" in all_config, f"Key 'Global' not found in config file. \n{all_config}"
     global_config = all_config["Global"]
-    gpu_num = paddle.distributed.get_world_size()
-    if all_config['TrainConfig']['learning_rate'][
-            'type'] == 'CosineAnnealingDecay':
-        step = int(
-            math.ceil(
-                float(args.total_images) / (global_config['batch_size'] *
-                                            gpu_num)))
-        all_config['TrainConfig']['learning_rate']['T_max'] = step
-        print('total training steps:', step)
     global data_dir
     data_dir = global_config['data_dir']
 
-    train_reader = paddle.batch(
-        reader.train(data_dir=data_dir), batch_size=global_config['batch_size'])
-    train_dataloader = reader_wrapper(train_reader, global_config['input_name'])
+    train_dataset = ImageNetDataset(mode='train', data_dir=data_dir)
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=global_config['batch_size'],
+        shuffle=True,
+        drop_last=True,
+        num_workers=0)
+    train_dataloader = reader_wrapper(train_loader, global_config['input_name'])
 
     ac = AutoCompression(
         model_dir=global_config['model_dir'],
@@ -142,10 +129,7 @@ def main():
         save_dir=args.save_dir,
         config=all_config,
         train_dataloader=train_dataloader,
-        eval_callback=eval_function,
-        eval_dataloader=reader_wrapper(
-            eval_reader(data_dir, global_config['batch_size']),
-            global_config['input_name']))
+        eval_callback=eval_function)
 
     ac.compress()
 
@@ -154,4 +138,5 @@ if __name__ == '__main__':
     paddle.enable_static()
     parser = argsparser()
     args = parser.parse_args()
+    print_arguments(args)
     main()
