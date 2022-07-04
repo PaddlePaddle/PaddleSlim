f6fdcc2c2fc737d588bbc435f9fe86d0436bf669
RachelXu7 <molixu7@gmail.com>
2022-07-04 11:20:21 +0000

    Fix Imagenet Demo

--- example/auto_compression/image_classification/eval.py
+++ example/auto_compression/image_classification/eval.py
@@ -14,8 +14,6 @@
 
 import os
 import sys
-sys.path[0] = os.path.join(
-    os.path.dirname("__file__"), os.path.pardir, os.path.pardir)
 import argparse
 import functools
 from functools import partial
