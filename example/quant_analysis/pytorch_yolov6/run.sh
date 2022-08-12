7b24892a75bf3d46c9e249e5e08f9c23ccc0937f
RachelXu7 <molixu7@gmail.com>
2022-08-12 09:36:00 +0000

    Add Quant Analyzer & YoLov6 Demo

--- example/quant_analysis/pytorch_yolov6/run.sh
+++ example/quant_analysis/pytorch_yolov6/run.sh
@@ -1,3 +1,1 @@
-export PYTHONPATH=/paddle/xuchang/Quant/PaddleSlim/:$PYTHONPATH
-#ps -aux | grep run | awk '{print $2}' | uniq | xargs kill -9
-python3.7 run.py --config_path=./config.yaml 
+python run.py --config_path=./config.yaml 
