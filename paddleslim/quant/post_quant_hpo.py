a49e09070c1020ed521d5e4f62c3f74ac8789e54
yghstill <742925032@qq.com>
2022-08-18 07:07:19 +0000

    lazy import smac

--- paddleslim/quant/post_quant_hpo.py
+++ paddleslim/quant/post_quant_hpo.py
@@ -29,13 +29,7 @@ import shutil
 import glob
 from scipy.stats import wasserstein_distance
 
-# smac
-from ConfigSpace.hyperparameters import CategoricalHyperparameter, \
-    UniformFloatHyperparameter, UniformIntegerHyperparameter
-from smac.configspace import ConfigurationSpace
-from smac.facade.smac_hpo_facade import SMAC4HPO
-from smac.scenario.scenario import Scenario
-
+import pkg_resources as pkg
 from paddleslim.common import get_logger
 from paddleslim.quant import quant_post
 
@@ -416,6 +410,18 @@ def quant_post_hpo(
     Returns:
         None
     """
+
+    try:
+        pkg.require('smac')
+    except:
+        from pip._internal import main
+        main(['install', 'smac'])
+    # smac
+    from ConfigSpace.hyperparameters import CategoricalHyperparameter, \
+        UniformFloatHyperparameter, UniformIntegerHyperparameter
+    from smac.configspace import ConfigurationSpace
+    from smac.facade.smac_hpo_facade import SMAC4HPO
+    from smac.scenario.scenario import Scenario
 
     global g_quant_config
     g_quant_config = QuantConfig(
