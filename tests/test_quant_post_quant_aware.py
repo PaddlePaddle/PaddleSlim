0b80d3e7ebcd71ab907eabda3d0e52a03ad92066
ZhangHandi <handi_zhang@163.com>
2022-08-26 06:00:12 +0000

    modifiy unittest to fix coverage ci

--- tests/test_quant_post_quant_aware.py
+++ tests/test_quant_post_quant_aware.py
@@ -1,7 +1,12 @@
 import sys
+import random
 sys.path.append("../")
 import unittest
 import paddle
+import paddle.nn as nn
+from paddle.io import Dataset
+from paddleslim.quant import quant_aware, convert
+from paddle.nn import TransformerEncoderLayer, TransformerEncoder, Linear
 from paddleslim.quant import quant_aware, convert
 from static_case import StaticCase
 sys.path.append("../demo")
@@ -12,18 +17,45 @@ from paddle.fluid.framework import IrGraph
 from paddle.fluid import core
 import numpy as np
 
+np.random.seed(0)
+random.seed(0)
+paddle.seed(0)
+
+
+class RandomDataset(Dataset):
+    def __init__(self, num_samples):
+        self.num_samples = num_samples
+
+    def __getitem__(self, idx):
+        enc_input = np.random.random([4, 128]).astype('float32')
+        attn_mask = np.random.random([2, 4, 4]).astype('float32')
+        label = np.random.randint(0, 2, (1, )).astype('int64')
+        return enc_input, attn_mask, label
+
+    def __len__(self):
+        return self.num_samples
+
 
 class TestQuantPostQuantAwareCase1(StaticCase):
     def test_accuracy(self):
-        image = paddle.static.data(
-            name='image', shape=[None, 1, 28, 28], dtype='float32')
+        def simple_transformer(enc_input, attn_mask):
+            encoder_layer = nn.TransformerEncoderLayer(128, 2, 512)
+            encoder = TransformerEncoder(encoder_layer, 2)
+            encoder_output = encoder(enc_input, attn_mask)
+            first_token = encoder_output[:, 0]
+            linear = Linear(128, 2)
+            logits = linear(first_token)
+            return logits
+
+        enc_input = paddle.static.data(
+            name='enc_input', shape=[None, 4, 128], dtype='float32')
+        attn_mask = paddle.static.data(
+            name='attn_mask', shape=[None, 2, 4, 4], dtype='float32')
         label = paddle.static.data(name='label', shape=[None, 1], dtype='int64')
-        model = MobileNet()
-        out = model.net(input=image, class_dim=10)
+        out = simple_transformer(enc_input, attn_mask)
         cost = paddle.nn.functional.loss.cross_entropy(input=out, label=label)
         avg_cost = paddle.mean(x=cost)
         acc_top1 = paddle.metric.accuracy(input=out, label=label, k=1)
-        acc_top5 = paddle.metric.accuracy(input=out, label=label, k=5)
         optimizer = paddle.optimizer.Momentum(
             momentum=0.9,
             learning_rate=0.01,
@@ -37,61 +69,52 @@ class TestQuantPostQuantAwareCase1(StaticCase):
         exe = paddle.static.Executor(place)
         exe.run(paddle.static.default_startup_program())
 
-        def transform(x):
-            return np.reshape(x, [1, 28, 28])
-
-        train_dataset = paddle.vision.datasets.MNIST(
-            mode='train', backend='cv2', transform=transform)
-        test_dataset = paddle.vision.datasets.MNIST(
-            mode='test', backend='cv2', transform=transform)
+        train_dataset = RandomDataset(100)
+        test_dataset = RandomDataset(50)
         train_loader = paddle.io.DataLoader(
             train_dataset,
             places=place,
-            feed_list=[image, label],
+            feed_list=[enc_input, attn_mask, label],
             drop_last=True,
             return_list=False,
-            batch_size=64)
+            batch_size=10)
         valid_loader = paddle.io.DataLoader(
             test_dataset,
             places=place,
-            feed_list=[image, label],
-            batch_size=64,
+            feed_list=[enc_input, attn_mask, label],
+            batch_size=10,
             return_list=False)
 
         def train(program):
             iter = 0
             for data in train_loader():
-                cost, top1, top5 = exe.run(
-                    program,
-                    feed=data,
-                    fetch_list=[avg_cost, acc_top1, acc_top5])
+                cost, top1 = exe.run(program,
+                                     feed=data,
+                                     fetch_list=[avg_cost, acc_top1])
                 iter += 1
                 if iter % 100 == 0:
-                    print(
-                        'train iter={}, avg loss {}, acc_top1 {}, acc_top5 {}'.
-                        format(iter, cost, top1, top5))
+                    print('train iter={}, avg loss {}, acc_top1 {}'.format(
+                        iter, cost, top1))
 
         def test(program):
             iter = 0
             result = [[], [], []]
             for data in valid_loader():
-                cost, top1, top5 = exe.run(
-                    program,
-                    feed=data,
-                    fetch_list=[avg_cost, acc_top1, acc_top5])
+                cost, top1 = exe.run(program,
+                                     feed=data,
+                                     fetch_list=[avg_cost, acc_top1])
                 iter += 1
                 if iter % 100 == 0:
-                    print('eval iter={}, avg loss {}, acc_top1 {}, acc_top5 {}'.
-                          format(iter, cost, top1, top5))
+                    print('eval iter={}, avg loss {}, acc_top1 {}'.format(
+                        iter, cost, top1))
                 result[0].append(cost)
                 result[1].append(top1)
-                result[2].append(top5)
-            print(' avg loss {}, acc_top1 {}, acc_top5 {}'.format(
-                np.mean(result[0]), np.mean(result[1]), np.mean(result[2])))
-            return np.mean(result[1]), np.mean(result[2])
+            print(' avg loss {}, acc_top1 {}'.format(
+                np.mean(result[0]), np.mean(result[1])))
+            return np.mean(result[1])
 
         train(main_prog)
-        top1_1, top5_1 = test(main_prog)
+        top1_1 = test(main_prog)
 
         config = {
             'weight_quantize_type': 'channel_wise_abs_max',
@@ -101,8 +124,8 @@ class TestQuantPostQuantAwareCase1(StaticCase):
         calib_config = {
             'data_loader': valid_loader,
             'algo': 'abs_max',
-            'feed_list': ['image', 'label'],
-            'fetch_list': [avg_cost, acc_top1, acc_top5]
+            'feed_list': ['enc_input', 'attn_mask', 'label'],
+            'fetch_list': [avg_cost, acc_top1]
         }
         quant_eval_prog, scale_dict, _, _ = quant_aware(
             val_prog,
@@ -110,7 +133,7 @@ class TestQuantPostQuantAwareCase1(StaticCase):
             config,
             for_test=True,
             calib_config=calib_config,
-            model_type='non-transformer',
+            model_type='transformer',
             return_scale_dict=True)
         quant_train_prog, _, _, _ = quant_aware(
             main_prog,
@@ -120,16 +143,16 @@ class TestQuantPostQuantAwareCase1(StaticCase):
             calib_config=calib_config,
             return_program=True,
             scale_dict=scale_dict,
-            model_type='non-transformer',
+            model_type='transformer',
             return_scale_dict=True)
 
         train(quant_train_prog)
         quant_eval_prog, int8_prog = convert(
             quant_eval_prog, place, config, save_int8=True)
-        top1_2, top5_2 = test(quant_eval_prog)
+        top1_2 = test(quant_eval_prog)
         # values before quantization and after quantization should be close
-        print("before quantization: top1: {}, top5: {}".format(top1_1, top5_1))
-        print("after quantization: top1: {}, top5: {}".format(top1_2, top5_2))
+        print("before quantization: top1: {}".format(top1_1))
+        print("after quantization: top1: {}".format(top1_2))
 
 
 if __name__ == '__main__':
