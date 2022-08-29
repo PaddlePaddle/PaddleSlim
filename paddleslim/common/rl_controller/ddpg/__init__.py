08f833503d2faea053121717902391da3f1633e4
ceci3 <592712189@qq.com>
2022-08-29 09:52:41 +0000

    fix parl version import error

--- paddleslim/common/rl_controller/ddpg/__init__.py
+++ paddleslim/common/rl_controller/ddpg/__init__.py
@@ -12,4 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .ddpg_controller import *
+import logging
+
+try:
+    from .ddpg_controller import *
+except Exception as e:
+    logging.warning(e)
