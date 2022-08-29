7473e1917671509cf73a3d5149070661003843a3
ceci3 <592712189@qq.com>
2022-08-29 13:43:19 +0000

    fix fake_ptq

--- paddleslim/auto_compression/utils/fake_ptq.py
+++ paddleslim/auto_compression/utils/fake_ptq.py
@@ -46,79 +46,81 @@ def post_quant_fake(executor,
         set(_weight_supported_quantizable_op_type +
             _act_supported_quantizable_op_type + _dynamic_quantize_op_type))
     _place = executor.place
-    _scope = paddle.static.global_scope()
-    if is_full_quantize:
-        _quantizable_op_type = _support_quantize_op_type
-    else:
-        _quantizable_op_type = quantizable_op_type
-        for op_type in _quantizable_op_type:
-            assert op_type in _support_quantize_op_type, \
-                op_type + " is not supported for quantization."
-    _program, _feed_list, _fetch_list = load_inference_model(
-        model_dir,
-        executor,
-        model_filename=model_filename,
-        params_filename=params_filename)
+    _scope = paddle.static.Scope()
 
-    graph = IrGraph(core.Graph(_program.desc), for_test=True)
+    with paddle.static.scope_guard(_scope):
+        if is_full_quantize:
+            _quantizable_op_type = _support_quantize_op_type
+        else:
+            _quantizable_op_type = quantizable_op_type
+            for op_type in _quantizable_op_type:
+                assert op_type in _support_quantize_op_type, \
+                    op_type + " is not supported for quantization."
+        _program, _feed_list, _fetch_list = load_inference_model(
+            model_dir,
+            executor,
+            model_filename=model_filename,
+            params_filename=params_filename)
 
-    # use QuantizationTransformPass to insert fake_quant/fake_dequantize op
-    major_quantizable_op_types = []
-    for op_type in _weight_supported_quantizable_op_type:
-        if op_type in _quantizable_op_type:
-            major_quantizable_op_types.append(op_type)
-    transform_pass = QuantizationTransformPass(
-        scope=_scope,
-        place=_place,
-        weight_bits=weight_bits,
-        activation_bits=activation_bits,
-        activation_quantize_type=activation_quantize_type,
-        weight_quantize_type=weight_quantize_type,
-        quantizable_op_type=major_quantizable_op_types)
+        graph = IrGraph(core.Graph(_program.desc), for_test=True)
 
-    for sub_graph in graph.all_sub_graphs():
-        # Insert fake_quant/fake_dequantize op must in test graph, so
-        # set per graph's _for_test is True.
-        sub_graph._for_test = True
-        transform_pass.apply(sub_graph)
+        # use QuantizationTransformPass to insert fake_quant/fake_dequantize op
+        major_quantizable_op_types = []
+        for op_type in _weight_supported_quantizable_op_type:
+            if op_type in _quantizable_op_type:
+                major_quantizable_op_types.append(op_type)
+        transform_pass = QuantizationTransformPass(
+            scope=_scope,
+            place=_place,
+            weight_bits=weight_bits,
+            activation_bits=activation_bits,
+            activation_quantize_type=activation_quantize_type,
+            weight_quantize_type=weight_quantize_type,
+            quantizable_op_type=major_quantizable_op_types)
 
-    # use AddQuantDequantPass to insert fake_quant_dequant op
-    minor_quantizable_op_types = []
-    for op_type in _act_supported_quantizable_op_type:
-        if op_type in _quantizable_op_type:
-            minor_quantizable_op_types.append(op_type)
-    add_quant_dequant_pass = AddQuantDequantPass(
-        scope=_scope,
-        place=_place,
-        quantizable_op_type=minor_quantizable_op_types)
+        for sub_graph in graph.all_sub_graphs():
+            # Insert fake_quant/fake_dequantize op must in test graph, so
+            # set per graph's _for_test is True.
+            sub_graph._for_test = True
+            transform_pass.apply(sub_graph)
 
-    for sub_graph in graph.all_sub_graphs():
-        sub_graph._for_test = True
-        add_quant_dequant_pass.apply(sub_graph)
+        # use AddQuantDequantPass to insert fake_quant_dequant op
+        minor_quantizable_op_types = []
+        for op_type in _act_supported_quantizable_op_type:
+            if op_type in _quantizable_op_type:
+                minor_quantizable_op_types.append(op_type)
+        add_quant_dequant_pass = AddQuantDequantPass(
+            scope=_scope,
+            place=_place,
+            quantizable_op_type=minor_quantizable_op_types)
 
-    # apply QuantizationFreezePass, and obtain the final quant model
-    freeze_pass = QuantizationFreezePass(
-        scope=_scope,
-        place=_place,
-        weight_bits=weight_bits,
-        activation_bits=activation_bits,
-        weight_quantize_type=weight_quantize_type,
-        quantizable_op_type=major_quantizable_op_types)
+        for sub_graph in graph.all_sub_graphs():
+            sub_graph._for_test = True
+            add_quant_dequant_pass.apply(sub_graph)
 
-    for sub_graph in graph.all_sub_graphs():
-        sub_graph._for_test = True
-        freeze_pass.apply(sub_graph)
+        # apply QuantizationFreezePass, and obtain the final quant model
+        freeze_pass = QuantizationFreezePass(
+            scope=_scope,
+            place=_place,
+            weight_bits=weight_bits,
+            activation_bits=activation_bits,
+            weight_quantize_type=weight_quantize_type,
+            quantizable_op_type=major_quantizable_op_types)
 
-    _program = graph.to_program()
+        for sub_graph in graph.all_sub_graphs():
+            sub_graph._for_test = True
+            freeze_pass.apply(sub_graph)
 
-    feed_vars = [_program.global_block().var(name) for name in _feed_list]
-    model_name = model_filename.split('.')[
-        0] if model_filename is not None else 'model'
-    save_model_path = os.path.join(save_model_path, model_name)
-    paddle.static.save_inference_model(
-        path_prefix=save_model_path,
-        feed_vars=feed_vars,
-        fetch_vars=_fetch_list,
-        executor=executor,
-        program=_program)
-    print("The quantized model is saved in: " + save_model_path)
+        _program = graph.to_program()
+
+        feed_vars = [_program.global_block().var(name) for name in _feed_list]
+        model_name = model_filename.split('.')[
+            0] if model_filename is not None else 'model'
+        save_model_path = os.path.join(save_model_path, model_name)
+        paddle.static.save_inference_model(
+            path_prefix=save_model_path,
+            feed_vars=feed_vars,
+            fetch_vars=_fetch_list,
+            executor=executor,
+            program=_program)
+        print("The quantized model is saved in: " + save_model_path)
