af0aab5ad141b0ee2aab72c6ef5ff946f9fc8d62
rehulisw <49474152+rehulisw@users.noreply.github.com>
2022-12-09 12:39:14 +0800

    Update itpruner.py

--- paddleslim/nas/itpruner/itpruner.py
+++ paddleslim/nas/itpruner/itpruner.py
@@ -54,15 +54,30 @@ class ITPruner:
                 a.append(x[i] * x[j] * self.flops_doublecfg[i][j])
         return np.array([self.target_flops - sum(a)])
 
+    def compute_similar_matrix(self, feature):
+        similar_matrix = np.zeros((len(feature), len(feature)))
+        for i in range(len(feature)):
+            for j in range(len(feature)):
+                similar_matrix[i][j] = cka.cka(cka.gram_linear(feature[i]), cka.gram_linear(feature[j]))
+        return similar_matrix
+    
+    def linear_programming(self):
+        bnds = []
+        for i in range(self.length):
+            bnds.append((0, 1))
+        bnds = tuple(bnds)
+        cons = ({'type': 'ineq',
+                 'fun': self.constrain_func})
+        result = optimize.minimize(self.func, x0=[1 for i in range(self.length)], jac=self.func_deriv, method='SLSQP', bounds=bnds,
+                                   constraints=cons)
+        return result
+
+    @paddle.no_grad()
     def prune(self, target_flops, beta):
         self.target_flops = target_flops
         temp = []
         feature = self.extract_feature(self.data)
-        similar_matrix = np.zeros((len(feature), len(feature)))
-        for i in range(len(feature)):
-            for j in range(len(feature)):
-                with paddle.no_grad():
-                    similar_matrix[i][j] = cka.cka(cka.gram_linear(feature[i]), cka.gram_linear(feature[j]))
+        similar_matrix = self.compute_similar_matrix(feature)
         for i in range(len(feature)):
             temp.append(sum_list(similar_matrix[i], i))
         b = sum_list(temp, -1)
@@ -73,20 +88,14 @@ class ITPruner:
         self.flops_singlecfg, self.flops_doublecfg, self.flops_squarecfg = self.net.cfg2flops_perlayer(self.net.cfg, self.length)
         self.important = np.array(self.important)
         self.important = np.negative(self.important)
-        bnds = []
-        for i in range(self.length):
-            bnds.append((0, 1))
-        bnds = tuple(bnds)
-        cons = ({'type': 'ineq',
-                 'fun': self.constrain_func})
-        result = optimize.minimize(self.func, x0=[1 for i in range(self.length)], jac=self.func_deriv, method='SLSQP', bounds=bnds,
-                                   constraints=cons)
-        prun_cfg = np.around(np.array(self.net.cfg) * result.x)
 
+        result = self.linear_programming()
+        prun_cfg = np.around(np.array(self.net.cfg) * result.x)
         optimize_cfg = []
         for i in range(len(prun_cfg)):
             b = list(prun_cfg)[i].tolist()
             optimize_cfg.append(int(b))
+
         print(optimize_cfg)
         print(self.net.cfg2flops(prun_cfg))
         print(self.net.cfg2flops(self.net.cfg))
