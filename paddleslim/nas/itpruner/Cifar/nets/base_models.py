0aaad6182cbc9943a2175869ecb2057e413f0a59
rehulisw <49474152+rehulisw@users.noreply.github.com>
2022-12-12 20:27:20 +0800

    Delete base_models.py

--- paddleslim/nas/itpruner/Cifar/nets/base_models.py
+++
@@ -1,9 +0,0 @@
-import paddle.nn as nn
-
-
-class MyNetwork(nn.Layer):
-    def feature_extract(self, x):
-        raise NotImplementedError
-
-    def cfg2flops(self, cfg):
-        raise NotImplementedError
