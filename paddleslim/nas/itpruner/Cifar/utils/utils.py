0f5c4b78ae57b76833bba884f19613916aced654
rehulisw <49474152+rehulisw@users.noreply.github.com>
2022-12-12 15:20:34 +0800

    Update utils.py

--- paddleslim/nas/itpruner/Cifar/utils/utils.py
+++ paddleslim/nas/itpruner/Cifar/utils/utils.py
@@ -1,8 +1,3 @@
-def count_parameters(model):
-    total_params = sum(p.numel() for p in model.parameters() if not p.stop_gradient)
-    return total_params
-
-
 def sum_list(a, j):
     b = 0
     for i in range(len(a)):
