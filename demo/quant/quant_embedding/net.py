92874cc00bd6c4218cf121317fd4c7ab11850dd3
zhouzj <41366441+zzjjay@users.noreply.github.com>
2023-01-31 10:56:43 +0800

    Clear fluid api and fix tests (#1641)
    * remove fluid apis.
    
    * fix hpo.
    
    * fix asp.

--- demo/quant/quant_embedding/net.py
+++ demo/quant/quant_embedding/net.py
@@ -15,15 +15,17 @@
 neural network for word2vec
 """
 from __future__ import print_function
-import math
-import numpy as np
 import paddle
-import paddle.fluid as fluid
+import paddle.nn.functional as F
 
 
-def skip_gram_word2vec(dict_size, embedding_size, is_sparse=False, neg_num=5):
+def skip_gram_word2vec(dict_size,
+                       embedding_size,
+                       batch_size,
+                       is_sparse=False,
+                       neg_num=5):
 
-    datas = []
+    words = []
     input_word = paddle.static.data(
         name="input_word", shape=[None, 1], dtype='int64')
     true_word = paddle.static.data(
@@ -31,14 +33,13 @@ def skip_gram_word2vec(dict_size, embedding_size, is_sparse=False, neg_num=5):
     neg_word = paddle.static.data(
         name="neg_label", shape=[None, neg_num], dtype='int64')
 
-    datas.append(input_word)
-    datas.append(true_word)
-    datas.append(neg_word)
+    words.append(input_word)
+    words.append(true_word)
+    words.append(neg_word)
 
-    py_reader = fluid.layers.create_py_reader_by_data(
-        capacity=64, feed_list=datas, name='py_reader', use_double_buffer=True)
+    py_reader = paddle.io.DataLoader.from_generator(
+        capacity=64, feed_list=words, use_double_buffer=True, iterable=False)
 
-    words = fluid.layers.read_file(py_reader)
     words[0] = paddle.reshape(words[0], [-1])
     words[1] = paddle.reshape(words[1], [-1])
     init_width = 0.5 / embedding_size
@@ -72,8 +73,7 @@ def skip_gram_word2vec(dict_size, embedding_size, is_sparse=False, neg_num=5):
         input=neg_word_reshape,
         is_sparse=is_sparse,
         size=[dict_size, embedding_size],
-        param_attr=paddle.ParamAttr(
-            name='emb_w', learning_rate=1.0))
+        param_attr=paddle.ParamAttr(name='emb_w', learning_rate=1.0))
 
     neg_emb_w_re = paddle.reshape(
         neg_emb_w, shape=[-1, neg_num, embedding_size])
@@ -81,31 +81,29 @@ def skip_gram_word2vec(dict_size, embedding_size, is_sparse=False, neg_num=5):
         input=neg_word_reshape,
         is_sparse=is_sparse,
         size=[dict_size, 1],
-        param_attr=paddle.ParamAttr(
-            name='emb_b', learning_rate=1.0))
+        param_attr=paddle.ParamAttr(name='emb_b', learning_rate=1.0))
 
     neg_emb_b_vec = paddle.reshape(neg_emb_b, shape=[-1, neg_num])
-    true_logits = paddle.add(paddle.mean(
-        paddle.multiply(input_emb, true_emb_w), keepdim=True),
-                             true_emb_b)
+    true_logits = paddle.add(
+        paddle.mean(paddle.multiply(input_emb, true_emb_w), keepdim=True),
+        true_emb_b)
     input_emb_re = paddle.reshape(input_emb, shape=[-1, 1, embedding_size])
     neg_matmul = paddle.matmul(input_emb_re, neg_emb_w_re, transpose_y=True)
     neg_matmul_re = paddle.reshape(neg_matmul, shape=[-1, neg_num])
     neg_logits = paddle.add(neg_matmul_re, neg_emb_b_vec)
     #nce loss
 
-    # TODO: replaced by paddle.tensor.creation.fill_constant_batch_size_like
-    label_ones = fluid.layers.fill_constant_batch_size_like(
-        true_logits, shape=[-1, 1], value=1.0, dtype='float32')
-    label_zeros = fluid.layers.fill_constant_batch_size_like(
-        true_logits, shape=[-1, neg_num], value=0.0, dtype='float32')
+    label_ones = paddle.full(
+        shape=[batch_size, 1], fill_value=1.0, dtype='float32')
+    label_zeros = paddle.full(
+        shape=[batch_size, neg_num], fill_value=0.0, dtype='float32')
 
-    true_xent = paddle.nn.functional.binary_cross_entropy(true_logits,
-                                                          label_ones)
-    neg_xent = paddle.nn.functional.binary_cross_entropy(neg_logits,
-                                                         label_zeros)
-    cost = paddle.add(paddle.sum(true_xent, axis=1),
-                      paddle.sum(neg_xent, axis=1))
+    true_xent = F.binary_cross_entropy_with_logits(
+        true_logits, label_ones, reduction='none')
+    neg_xent = F.binary_cross_entropy_with_logits(
+        neg_logits, label_zeros, reduction='none')
+    cost = paddle.add(
+        paddle.sum(true_xent, axis=1), paddle.sum(neg_xent, axis=1))
     avg_cost = paddle.mean(cost)
     return avg_cost, py_reader
 
