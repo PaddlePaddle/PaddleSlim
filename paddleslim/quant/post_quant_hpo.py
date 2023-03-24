2fd409628cb610a967849458621b2f1ad4807c99
zhouzj <41366441+zzjjay@users.noreply.github.com>
2023-03-24 10:25:18 +0800

    Update SMAC version requirements (#1684)

--- paddleslim/quant/post_quant_hpo.py
+++ paddleslim/quant/post_quant_hpo.py
@@ -415,9 +415,12 @@ def quant_post_hpo(
 
     try:
         import smac
+        assert smac.version == '1.4.0'
     except:
-        os.system('python -m pip install -U smac')
-    # smac
+        _logger.warning(
+            "smac==1.4.0 is required, please use \"pip install smac==1.4.0\".")
+        os.system('python -m pip install smac==1.4.0')
+
     from ConfigSpace.hyperparameters import CategoricalHyperparameter, \
         UniformFloatHyperparameter, UniformIntegerHyperparameter
     from smac.configspace import ConfigurationSpace
