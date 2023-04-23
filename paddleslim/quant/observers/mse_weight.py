4e22bac9d7bde543b6db8e3c0701e1cdd64c56b0
RachelXu7 <molixu7@gmail.com>
2023-04-23 10:56:08 +0000

    Add CastFP32 to Avoid Dtype Mismatching

--- paddleslim/quant/observers/mse_weight.py
+++ paddleslim/quant/observers/mse_weight.py
@@ -49,7 +49,7 @@ class MSEChannelWiseWeightObserverLayer(AbsMaxChannelWiseWeightObserverLayer):
             layer, quant_bits=quant_bits)
 
     def _cal_abs_max(self, inputs):
-
+        inputs = inputs.cast('float32')
         reduce_axis = tuple(
             [i for i in range(len(inputs.shape)) if i != self.quant_axis()])
         abs_max_values = paddle.max(paddle.abs(inputs), axis=reduce_axis)
