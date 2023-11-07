c472def779f7da1f905783dfb282ec7568493eeb
ceci3 <592712189@qq.com>
2023-11-07 06:02:53 +0000

    update license

--- paddleslim/lc/layers/linear.py
+++ paddleslim/lc/layers/linear.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import paddle
 import paddle.nn as nn
 
@@ -35,14 +48,6 @@ class Linear4bit(WeightQuantizationLinear):
             attr=paddle.nn.initializer.Constant(value=0),
             dtype=self.weight_dtype,
             is_bias=False, )
-
-        ###self.quant_scale_name = ".".join(
-        ###    [self.weight_name, quant_type + self.quant_scale_suffix])
-        ###self.quant_scale = self.create_parameter(
-        ###    shape=[self.out_features],
-        ###    attr=paddle.ParamAttr(self.quant_scale_name),
-        ###    dtype="float32",  # to be fixed
-        ###    is_bias=False, )
 
     def forward(self, x):
         raise NotImplementedError()
